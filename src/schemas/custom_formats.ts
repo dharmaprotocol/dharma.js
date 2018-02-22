4b5e9d38d2a4cf2733a0d985c489ae08e72bf4b8
Graeme Boy <graemelearn@protonmail.com>
2018-02-22 13:56:17 -0800

    Be clearer in custom format comment

--- src/schemas/custom_formats.ts
+++ src/schemas/custom_formats.ts
@@ -1,8 +1,8 @@
 export const bigNumberFormat = function(input) {
     const regex = RegExp("^\\d+(\\.\\d+)?$");
-    // The input's format only matters if the input is required, in which case
-    // the validator will throw an error for an undefined input at another point. Hence, we do not
-    // need to validate presence here.
+    // The input's format only matters if the input is required (in which case
+    // an undefined value will cause the validator to throw an error at another point.)
+    // Hence, we do not need to validate presence here.
     return input === undefined || (input.isBigNumber && regex.test(input.toString()));
 };
 
