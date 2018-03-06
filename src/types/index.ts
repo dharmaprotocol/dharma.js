9a9039cb69d785736e5fa05091d737951b520c55
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-06 15:59:35 -0800

    Export these types to be consumed in the main app

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,5 +4,15 @@ import { ECDSASignature } from "./ecdsa_signature";
 import { IssuanceCommitment } from "./issuance_commitment";
 import { TxData, TxDataPayable } from "./transaction_options";
 import { DharmaConfig } from "./dharma_config";
+import { DebtRegistryEntry, DebtRegistryEntryData } from "./debt_registry_entry";
 
-export { DebtOrder, DharmaConfig, ECDSASignature, IssuanceCommitment, TxData, TxDataPayable };
+export {
+    DebtOrder,
+    DharmaConfig,
+    ECDSASignature,
+    IssuanceCommitment,
+    DebtRegistryEntry,
+    DebtRegistryEntryData,
+    TxData,
+    TxDataPayable,
+};
