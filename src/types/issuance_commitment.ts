73f410d119457180a661e4c4d15e83a60c5373a2
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-05 17:00:39 -0800

    Make `IssuanceCommitment` a class so we can create them

--- src/types/issuance_commitment.ts
+++ src/types/issuance_commitment.ts
@@ -1,11 +1,32 @@
 import { BigNumber } from "../../utils/bignumber";
 
-export interface IssuanceCommitment {
-    version: string;
-    beneficiary: string;
-    underwriter: string;
-    underwriterRiskRating: BigNumber;
-    termsContract: string;
-    termsContractParameters: string;
-    issuanceBlockTimestamp: BigNumber;
+export class IssuanceCommitment {
+    public version: string;
+    public beneficiary: string;
+    public underwriter: string;
+    public underwriterRiskRating: BigNumber;
+    public termsContract: string;
+    public termsContractParameters: string;
+    public issuanceBlockTimestamp: BigNumber;
+
+    constructor(
+        version: string,
+        beneficiary: string,
+        underwriter: string,
+        underwriterRiskRating: BigNumber,
+        termsContract: string,
+        termsContractParameters: string,
+        issuanceBlockTimestamp: BigNumber,
+    ) {
+        this.version = version;
+        this.beneficiary = beneficiary;
+        this.underwriter = underwriter;
+        this.underwriterRiskRating = underwriterRiskRating;
+        this.termsContract = termsContract;
+        this.termsContractParameters = termsContractParameters;
+        this.issuanceBlockTimestamp = issuanceBlockTimestamp;
+    }
+    static fromData(data: [string, string, string, BigNumber, string, string, BigNumber]) {
+        return new this(data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
+    }
 }
