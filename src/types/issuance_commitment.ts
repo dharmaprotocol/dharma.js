3bef4896973bd51fadd08f36a658d6defed3213e
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-05 18:41:41 -0800

    `IssuanceCommitment` can compute its own hash

--- src/types/issuance_commitment.ts
+++ src/types/issuance_commitment.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from "../../utils/bignumber";
+import { Web3Utils } from "../../utils/web3_utils";
 
 export type IssuanceCommitmentData = [string, string, string, BigNumber, string, string, BigNumber];
 
@@ -20,6 +21,19 @@ export class IssuanceCommitment {
         this.termsContractParameters = termsContractParameters;
         this.issuanceBlockTimestamp = issuanceBlockTimestamp;
     }
+
+    public getHash(): string {
+        return Web3Utils.soliditySHA3(
+            this.version,
+            this.beneficiary,
+            this.underwriter,
+            this.underwriterRiskRating,
+            this.termsContract,
+            this.termsContractParameters,
+            this.issuanceBlockTimestamp,
+        );
+    }
+
     static fromData(data: IssuanceCommitmentData) {
         return new this(data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
     }
