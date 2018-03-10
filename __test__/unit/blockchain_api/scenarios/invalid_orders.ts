65599b88f853b33f90fc5061d965038f87635f34
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-09 16:04:37 -0800

    test: codify first scenario as `DebtKernlErrorScenario`

---
+++ __test__/unit/blockchain_api/scenarios/invalid_orders.ts
@@ -0,0 +1,52 @@
+import * as Units from "utils/units";
+import * as moment from "moment";
+import { BigNumber } from "bignumber.js";
+import { ACCOUNTS } from "__test__/accounts";
+import { NULL_BYTES32 } from "utils/constants";
+
+import { DebtKernelError } from "src/types";
+import {
+    DebtKernelContract,
+    RepaymentRouterContract,
+    DummyTokenContract,
+    SimpleInterestTermsContractContract,
+} from "src/wrappers";
+
+import { DebtKernlErrorScenario } from "./error_scenarios";
+
+export const INVALID_ORDERS: DebtKernlErrorScenario[] = [
+    {
+        description: "with principal < debtor fee",
+        generateDebtOrder: (
+            debtKernel: DebtKernelContract,
+            repaymentRouter: RepaymentRouterContract,
+            principalToken: DummyTokenContract,
+            termsContract: SimpleInterestTermsContractContract,
+        ) => {
+            return {
+                kernelVersion: debtKernel.address,
+                issuanceVersion: repaymentRouter.address,
+                principalAmount: Units.ether(0.49),
+                principalToken: principalToken.address,
+                debtor: ACCOUNTS[1].address,
+                debtorFee: Units.ether(0.51),
+                creditor: ACCOUNTS[2].address,
+                creditorFee: Units.ether(0.001),
+                relayer: ACCOUNTS[3].address,
+                relayerFee: Units.ether(0.001),
+                underwriter: ACCOUNTS[4].address,
+                underwriterFee: Units.ether(0.511),
+                underwriterRiskRating: Units.percent(0.001),
+                termsContract: termsContract.address,
+                termsContractParameters: NULL_BYTES32,
+                expirationTimestampInSec: new BigNumber(
+                    moment()
+                        .add(7, "days")
+                        .unix(),
+                ),
+                salt: new BigNumber(0),
+            };
+        },
+        error: DebtKernelError.ORDER_INVALID_INSUFFICIENT_OR_EXCESSIVE_FEES,
+    },
+];
