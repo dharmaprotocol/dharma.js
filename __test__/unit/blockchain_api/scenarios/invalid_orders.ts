5a32f0be5c6745028904eb516e2a97d16ded2ee9
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-09 17:10:49 -0800

    test: add scenario for underwriter fee specified with no underwriter

--- __test__/unit/blockchain_api/scenarios/invalid_orders.ts
+++ __test__/unit/blockchain_api/scenarios/invalid_orders.ts
@@ -2,7 +2,7 @@ import * as Units from "utils/units";
 import * as moment from "moment";
 import { BigNumber } from "bignumber.js";
 import { ACCOUNTS } from "__test__/accounts";
-import { NULL_BYTES32 } from "utils/constants";
+import { NULL_BYTES32, NULL_ADDRESS } from "utils/constants";
 
 import { DebtKernelError } from "src/types";
 import {
@@ -48,5 +48,39 @@ export const INVALID_ORDERS: DebtKernelErrorScenario[] = [
             };
         },
         error: DebtKernelError.ORDER_INVALID_INSUFFICIENT_PRINCIPAL,
+    },
+    {
+        description: "with no underwriter but with underwriter fee",
+        generateDebtOrder: (
+            debtKernel: DebtKernelContract,
+            repaymentRouter: RepaymentRouterContract,
+            principalToken: DummyTokenContract,
+            termsContract: SimpleInterestTermsContractContract,
+        ) => {
+            return {
+                kernelVersion: debtKernel.address,
+                issuanceVersion: repaymentRouter.address,
+                principalAmount: Units.ether(1),
+                principalToken: principalToken.address,
+                debtor: ACCOUNTS[1].address,
+                debtorFee: Units.ether(0.001),
+                creditor: ACCOUNTS[2].address,
+                creditorFee: Units.ether(0.001),
+                relayer: ACCOUNTS[3].address,
+                relayerFee: Units.ether(0.001),
+                underwriter: NULL_ADDRESS,
+                underwriterFee: Units.ether(0.001),
+                underwriterRiskRating: Units.percent(0.001),
+                termsContract: termsContract.address,
+                termsContractParameters: NULL_BYTES32,
+                expirationTimestampInSec: new BigNumber(
+                    moment()
+                        .add(7, "days")
+                        .unix(),
+                ),
+                salt: new BigNumber(0),
+            };
+        },
+        error: DebtKernelError.ORDER_INVALID_UNSPECIFIED_FEE_RECIPIENT,
     },
 ];
