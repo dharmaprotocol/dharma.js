fd45ce960a95623b28243a4b551cbcd0791ccd83
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-12 18:02:37 -0700

    test: add scenario for when creditor balance is insufficient

--- __test__/unit/blockchain_api/scenarios/invalid_orders.ts
+++ __test__/unit/blockchain_api/scenarios/invalid_orders.ts
@@ -251,6 +251,46 @@ export const INVALID_ORDERS: DebtKernelErrorScenario[] = [
         },
     },
     {
+        description: "insufficient balance",
+        generateDebtOrder: (
+            debtKernel: DebtKernelContract,
+            repaymentRouter: RepaymentRouterContract,
+            principalToken: DummyTokenContract,
+            termsContract: SimpleInterestTermsContractContract,
+        ) => {
+            return {
+                kernelVersion: debtKernel.address,
+                issuanceVersion: repaymentRouter.address,
+                principalAmount: Units.ether(1),
+                principalToken: principalToken.address,
+                debtor: ACCOUNTS[1].address,
+                debtorFee: Units.ether(0.001),
+                creditor: ACCOUNTS[2].address,
+                creditorFee: Units.ether(0.001),
+                relayer: ACCOUNTS[3].address,
+                relayerFee: Units.ether(0.001),
+                underwriter: ACCOUNTS[4].address,
+                underwriterFee: Units.ether(0.001),
+                underwriterRiskRating: Units.percent(0.001),
+                termsContract: termsContract.address,
+                termsContractParameters: NULL_BYTES32,
+                expirationTimestampInSec: new BigNumber(
+                    moment()
+                        .add(7, "days")
+                        .unix(),
+                ),
+                salt: new BigNumber(0),
+            };
+        },
+        error: DebtKernelError.CREDITOR_BALANCE_OR_ALLOWANCE_INSUFFICIENT,
+        creditorBalance: Units.ether(0),
+        signatories: {
+            debtor: true,
+            creditor: false,
+            underwriter: true,
+        },
+    },
+    {
         description: "order has been cancelled",
         generateDebtOrder: (
             debtKernel: DebtKernelContract,
