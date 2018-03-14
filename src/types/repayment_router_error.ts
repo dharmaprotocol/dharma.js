6ca941d4b9c1735989f0f47efa1b9ac1d3c12d1c
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-13 17:58:16 -0700

    fix: rename `ReypaymentRouterError` -> `RepaymentRouterError`

--- src/types/repayment_router_error.ts
+++ src/types/repayment_router_error.ts
@@ -1,22 +1,22 @@
 import * as singleLineString from "single-line-string";
 
-export enum ReypaymentRouterError {
+export enum RepaymentRouterError {
     DEBT_AGREEMENT_NONEXISTENT,
     PAYER_BALANCE_OR_ALLOWANCE_INSUFFICIENT,
     PAYER_OWNERSHIP_OR_ROUTER_APPROVAL_MISSING,
     ROUTER_UNAUTHORIZED_TO_REPORT_REPAYMENT,
 }
 
-export namespace ReypaymentRouterError {
-    export function messageForError(error: ReypaymentRouterError): string {
+export namespace RepaymentRouterError {
+    export function messageForError(error: RepaymentRouterError): string {
         switch (error) {
-            case ReypaymentRouterError.DEBT_AGREEMENT_NONEXISTENT:
+            case RepaymentRouterError.DEBT_AGREEMENT_NONEXISTENT:
                 return singleLineString`Debt agreement does not exist.`;
-            case ReypaymentRouterError.PAYER_BALANCE_OR_ALLOWANCE_INSUFFICIENT:
+            case RepaymentRouterError.PAYER_BALANCE_OR_ALLOWANCE_INSUFFICIENT:
                 return singleLineString`Payer does not have sufficient balance or allowance.`;
-            case ReypaymentRouterError.PAYER_OWNERSHIP_OR_ROUTER_APPROVAL_MISSING:
+            case RepaymentRouterError.PAYER_OWNERSHIP_OR_ROUTER_APPROVAL_MISSING:
                 return singleLineString`Payer has not granted sufficient approvals to route this payment.`;
-            case ReypaymentRouterError.ROUTER_UNAUTHORIZED_TO_REPORT_REPAYMENT:
+            case RepaymentRouterError.ROUTER_UNAUTHORIZED_TO_REPORT_REPAYMENT:
                 return singleLineString`Router unauthorized to report repayment.`;
         }
     }
