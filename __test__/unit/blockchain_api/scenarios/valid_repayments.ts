137bcfe89c7138ebfc1a49246262f559e2e6d808
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-16 14:28:21 -0700

    test: define valid repayment scenarios

---
+++ __test__/unit/blockchain_api/scenarios/valid_repayments.ts
@@ -0,0 +1,7 @@
+import { RepaymentRouterErrorScenario } from "./";
+
+export const VALID_REPAYMENTS: RepaymentRouterErrorScenario[] = [
+    {
+        description: "a successful repayment is conducted",
+    },
+];
