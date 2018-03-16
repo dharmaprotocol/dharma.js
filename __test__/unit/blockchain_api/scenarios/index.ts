69d97f8c81422ad882e5499a07ba050d0c487e56
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-16 14:29:57 -0700

    test: export our valid repayment scenarios

--- __test__/unit/blockchain_api/scenarios/index.ts
+++ __test__/unit/blockchain_api/scenarios/index.ts
@@ -1,5 +1,6 @@
 import { DebtKernelErrorScenario } from "./error_scenarios";
 import { INVALID_ORDERS } from "./invalid_orders";
 import { VALID_ORDERS } from "./valid_orders";
+import { VALID_REPAYMENTS } from "./valid_repayments";
 
-export { DebtKernelErrorScenario, INVALID_ORDERS, VALID_ORDERS };
+export { DebtKernelErrorScenario, INVALID_ORDERS, VALID_ORDERS, VALID_REPAYMENTS };
