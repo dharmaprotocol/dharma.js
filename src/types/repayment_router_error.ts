3d9fc1c9d9b7710dc1c6f04bd69b9d8c7541de30
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-16 18:50:17 -0700

    refactor: remove deprecated error

--- src/types/repayment_router_error.ts
+++ src/types/repayment_router_error.ts
@@ -3,7 +3,6 @@ import * as singleLineString from "single-line-string";
 export enum RepaymentRouterError {
     DEBT_AGREEMENT_NONEXISTENT,
     PAYER_BALANCE_OR_ALLOWANCE_INSUFFICIENT,
-    PAYER_OWNERSHIP_OR_ROUTER_APPROVAL_MISSING,
     ROUTER_UNAUTHORIZED_TO_REPORT_REPAYMENT,
 }
 
@@ -14,8 +13,6 @@ export namespace RepaymentRouterError {
                 return singleLineString`Debt agreement does not exist.`;
             case RepaymentRouterError.PAYER_BALANCE_OR_ALLOWANCE_INSUFFICIENT:
                 return singleLineString`Payer does not have sufficient balance or allowance.`;
-            case RepaymentRouterError.PAYER_OWNERSHIP_OR_ROUTER_APPROVAL_MISSING:
-                return singleLineString`Payer has not granted sufficient approvals to route this payment.`;
             case RepaymentRouterError.ROUTER_UNAUTHORIZED_TO_REPORT_REPAYMENT:
                 return singleLineString`Router unauthorized to report repayment.`;
         }
