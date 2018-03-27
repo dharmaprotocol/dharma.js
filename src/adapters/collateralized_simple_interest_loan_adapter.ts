769733ba132d4a2284898ff9cb24b83d463cdea1
Kayvon Tehranian <ktehranian@gmail.com>
2018-03-27 14:41:05 -0700

    feat: add utility function `generateHexValueOfLength`

--- src/adapters/collateralized_simple_interest_loan_adapter.ts
+++ src/adapters/collateralized_simple_interest_loan_adapter.ts
@@ -11,6 +11,10 @@ export interface CollateralizedTermsContractParameters {
 }
 
 export namespace TermsContractParameters {
+    export function generateHexValueOfLength(length: number): string {
+        return "0x" + "f".repeat(length);
+    }
+
     export function bitShiftLeft(target: BigNumber, numPlaces: number): BigNumber {
         const binaryTargetString = target.toString(2);
         const binaryTargetStringShifted = binaryTargetString + "0".repeat(numPlaces);
