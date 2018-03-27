44b3be066ae58b7ba8bc816cc560922caf7c82b3
Nadav Hollander <nadav@dharma.io>
2018-03-26 17:34:15 -0700

    refactor: change signatureUtils from object to class

--- utils/signature_utils.ts
+++ utils/signature_utils.ts
@@ -1,19 +1,30 @@
-import * as ethUtil from 'ethereumjs-util';
+import * as ethUtil from "ethereumjs-util";
 
-import { ECDSASignature } from '../src/types';
+import { ECDSASignature } from "../src/types";
 
 /*
  * Ver batim copied, with slight modifications, from the wonderful 0x.js project codebase:
  * https://github.com/0xProject/0x.js
  */
 
-export const signatureUtils = {
-    isValidSignature(data: string, signature: ECDSASignature, signerAddress: string): boolean {
-        const dataBuff = ethUtil.toBuffer(data);
-        const msgHashBuff = ethUtil.hashPersonalMessage(dataBuff);
+export class SignatureUtils {
+    public static isValidSignature(
+        data: string,
+        signature: ECDSASignature,
+        signerAddress: string,
+        addPersonalMessagePrefix: boolean = true,
+    ): boolean {
+        let messageHash = data;
+
+        if (addPersonalMessagePrefix) {
+            messageHash = SignatureUtils.addPersonalMessagePrefix(messageHash);
+        }
+
+        const messageHashBuff = ethUtil.toBuffer(messageHash);
+
         try {
             const pubKey = ethUtil.ecrecover(
-                msgHashBuff,
+                messageHashBuff,
                 signature.v,
                 ethUtil.toBuffer(signature.r),
                 ethUtil.toBuffer(signature.s),
@@ -23,9 +34,16 @@ export const signatureUtils = {
         } catch (err) {
             return false;
         }
-    },
+    }
 
-    parseSignatureHexAsVRS(signatureHex: string): ECDSASignature {
+    public static addPersonalMessagePrefix(messageHashHex: string): string {
+        const messageHashBuffer = ethUtil.toBuffer(messageHashHex);
+        const prefixedMessageHashBuffer = ethUtil.hashPersonalMessage(messageHashBuffer);
+
+        return ethUtil.bufferToHex(prefixedMessageHashBuffer);
+    }
+
+    public static parseSignatureHexAsVRS(signatureHex: string): ECDSASignature {
         const signatureBuffer = ethUtil.toBuffer(signatureHex);
         let v = signatureBuffer[0];
         if (v < 27) {
@@ -39,9 +57,9 @@ export const signatureUtils = {
             s: ethUtil.bufferToHex(s),
         };
         return ECDSASignature;
-    },
+    }
 
-    parseSignatureHexAsRSV(signatureHex: string): ECDSASignature {
+    public static parseSignatureHexAsRSV(signatureHex: string): ECDSASignature {
         const { v, r, s } = ethUtil.fromRpcSig(signatureHex);
         const ECDSASignature: ECDSASignature = {
             v,
@@ -49,25 +67,15 @@ export const signatureUtils = {
             s: ethUtil.bufferToHex(s),
         };
         return ECDSASignature;
-    },
-
-    convertToHexRSV(signature: ECDSASignature): string {
-        const { r, s, v } = signature;
-        return [
-            "0x",
-            r,
-            s,
-            v.toString(16),
-        ].join("");
-    },
-
-    convertToHexVRS(signature: ECDSASignature): string {
-        const { v, r, s } = signature;
-        return [
-            "0x",
-            v.toString(16),
-            r,
-            s,
-        ].join("");
     }
-};
+
+    public static convertToHexRSV(signature: ECDSASignature): string {
+        const { r, s, v } = signature;
+        return ["0x", r, s, v.toString(16)].join("");
+    }
+
+    public static convertToHexVRS(signature: ECDSASignature): string {
+        const { v, r, s } = signature;
+        return ["0x", v.toString(16), r, s].join("");
+    }
+}
