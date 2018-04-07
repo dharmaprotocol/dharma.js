c32b9847b3856d2ee52858c65a3d836757e29afe
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-06 17:09:28 -0700

    test: run scenario in spec

--- __test__/integration/debt_token_api/debt_token_api.spec.ts
+++ __test__/integration/debt_token_api/debt_token_api.spec.ts
@@ -8,6 +8,8 @@ import * as Web3 from "web3";
 
 import { DebtTokenScenarioRunner } from "./debt_token_scenario_runner";
 
+import { BALANCE_OF_SCENARIOS } from "./scenarios";
+
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const web3 = new Web3(provider);
 
@@ -15,10 +17,16 @@ const scenarioRunner = new DebtTokenScenarioRunner(web3);
 
 describe("Debt Token API (Integration Tests)", () => {
     beforeAll(async () => {
-        scenarioRunner.configure();
+        await scenarioRunner.configure();
     });
 
     beforeEach(scenarioRunner.saveSnapshotAsync);
 
     afterEach(scenarioRunner.revertToSavedSnapshot);
+
+    describe("#balanceOf", () => {
+        describe("debt token balances should be retrievable", () => {
+            BALANCE_OF_SCENARIOS.forEach(scenarioRunner.testBalanceOfScenario);
+        });
+    });
 });
