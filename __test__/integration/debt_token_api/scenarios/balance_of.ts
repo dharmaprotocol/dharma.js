a53a8e72b8a8785f96ad7a20fbee32d1999ec599
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-08 18:04:10 -0700

    refactor: create namespace for orders

--- __test__/integration/debt_token_api/scenarios/balance_of.ts
+++ __test__/integration/debt_token_api/scenarios/balance_of.ts
@@ -1,6 +1,6 @@
 import { DebtTokenScenario } from "./scenarios";
 import * as singleLineString from "single-line-string";
-import { ORDERS, CREDITOR, DEBTOR } from "./orders";
+import { Orders } from "./orders";
 
 const DESCRIPTION = (balance: number) =>
     singleLineString`when user holds a balance of ${balance} debt token${
@@ -10,14 +10,14 @@ const DESCRIPTION = (balance: number) =>
 function generateScenarioForBalanceOf(balance: number): DebtTokenScenario.BalanceOfScenario {
     return {
         description: DESCRIPTION(balance),
-        orders: ORDERS.slice(0, balance),
-        creditor: CREDITOR,
-        debtor: DEBTOR,
+        orders: Orders.ALL_ORDERS.slice(0, balance),
+        creditor: Orders.CREDITOR,
+        debtor: Orders.DEBTOR,
         balance: balance,
     };
 }
 
-const BALANCES = Array.from(Array(ORDERS.length + 1).keys()); // [0, 1, 2, 3]
+const BALANCES = Array.from(Array(Orders.ALL_ORDERS.length + 1).keys()); // [0, 1, 2, 3]
 
 export const BALANCE_OF_SCENARIOS: DebtTokenScenario.BalanceOfScenario[] = BALANCES.map(
     generateScenarioForBalanceOf,
--- __test__/integration/debt_token_api/scenarios/orders.ts
+++ __test__/integration/debt_token_api/scenarios/orders.ts
@@ -3,37 +3,39 @@ import { SimpleInterestLoanOrder } from "src/adapters/simple_interest_loan_adapt
 import { BigNumber } from "bignumber.js";
 import { ERC20TokenSymbol } from "utils/constants";
 
-export const CREDITOR = ACCOUNTS[0].address;
-export const DEBTOR = ACCOUNTS[1].address;
+export namespace Orders {
+    export const CREDITOR = ACCOUNTS[0].address;
+    export const DEBTOR = ACCOUNTS[1].address;
 
-export const ORDER_ONE: SimpleInterestLoanOrder = {
-    principalAmount: new BigNumber(10 * 10 ** 18),
-    principalTokenSymbol: ERC20TokenSymbol.ZRX,
-    interestRate: new BigNumber(4.135),
-    amortizationUnit: "months",
-    termLength: new BigNumber(3),
-    debtor: DEBTOR,
-    creditor: CREDITOR,
-};
+    export const ORDER_ONE: SimpleInterestLoanOrder = {
+        principalAmount: new BigNumber(10 * 10 ** 18),
+        principalTokenSymbol: ERC20TokenSymbol.ZRX,
+        interestRate: new BigNumber(4.135),
+        amortizationUnit: "months",
+        termLength: new BigNumber(3),
+        debtor: DEBTOR,
+        creditor: CREDITOR,
+    };
 
-export const ORDER_TWO: SimpleInterestLoanOrder = {
-    principalAmount: new BigNumber(11 * 10 ** 18),
-    principalTokenSymbol: ERC20TokenSymbol.MKR,
-    interestRate: new BigNumber(8.937),
-    amortizationUnit: "years",
-    termLength: new BigNumber(2),
-    debtor: DEBTOR,
-    creditor: CREDITOR,
-};
+    export const ORDER_TWO: SimpleInterestLoanOrder = {
+        principalAmount: new BigNumber(11 * 10 ** 18),
+        principalTokenSymbol: ERC20TokenSymbol.MKR,
+        interestRate: new BigNumber(8.937),
+        amortizationUnit: "years",
+        termLength: new BigNumber(2),
+        debtor: DEBTOR,
+        creditor: CREDITOR,
+    };
 
-export const ORDER_THREE: SimpleInterestLoanOrder = {
-    principalAmount: new BigNumber(12 * 10 ** 18),
-    principalTokenSymbol: ERC20TokenSymbol.REP,
-    interestRate: new BigNumber(1.987),
-    amortizationUnit: "weeks",
-    termLength: new BigNumber(10),
-    debtor: DEBTOR,
-    creditor: CREDITOR,
-};
+    export const ORDER_THREE: SimpleInterestLoanOrder = {
+        principalAmount: new BigNumber(12 * 10 ** 18),
+        principalTokenSymbol: ERC20TokenSymbol.REP,
+        interestRate: new BigNumber(1.987),
+        amortizationUnit: "weeks",
+        termLength: new BigNumber(10),
+        debtor: DEBTOR,
+        creditor: CREDITOR,
+    };
 
-export const ORDERS = [ORDER_ONE, ORDER_TWO, ORDER_THREE];
+    export const ALL_ORDERS = [ORDER_ONE, ORDER_TWO, ORDER_THREE];
+}
