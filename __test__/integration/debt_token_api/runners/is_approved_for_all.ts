e2368035e85730a6d45b1f24eb378bba81130f2d
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-11 15:29:13 -0700

    test: introduce runner for `isApprovedForAll` scenarios

---
+++ __test__/integration/debt_token_api/runners/is_approved_for_all.ts
@@ -0,0 +1,32 @@
+import { ScenarioRunner } from "./";
+import { DebtTokenScenario } from "../scenarios";
+
+export class IsApprovedForAllScenarioRunner extends ScenarioRunner {
+    public testScenario(scenario: DebtTokenScenario.IsApprovedForAllScenario) {
+        const { debtTokenAPI } = this.testAPIs;
+
+        let apiCall: Promise<boolean>;
+
+        describe(scenario.description, () => {
+            beforeEach(async () => {
+                if (scenario.isApproved) {
+                    await debtTokenAPI.setApprovalForAll(scenario.operator, true, {
+                        from: scenario.owner,
+                    });
+                }
+
+                apiCall = debtTokenAPI.isApprovedForAll(scenario.owner, scenario.operator);
+            });
+
+            if (scenario.shouldSucceed) {
+                test("returns status of operator vis-a-vis owner", async () => {
+                    await expect(apiCall).resolves.toBe(scenario.isApproved);
+                });
+            } else {
+                test(`throws ${scenario.errorType}`, async () => {
+                    await expect(apiCall).rejects.toThrow(scenario.errorMessage);
+                });
+            }
+        });
+    }
+}
