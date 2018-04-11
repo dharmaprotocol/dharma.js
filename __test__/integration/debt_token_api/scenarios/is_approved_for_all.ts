bff38401c6845a4addc25ebc1805e9395f803e85
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-11 15:41:57 -0700

    test: define scenarios for `isApprovedForAll`

---
+++ __test__/integration/debt_token_api/scenarios/is_approved_for_all.ts
@@ -0,0 +1,40 @@
+import { DebtTokenScenario } from "./scenarios";
+import { ACCOUNTS } from "__test__/accounts";
+
+const OWNER = ACCOUNTS[0].address;
+const OPERATOR = ACCOUNTS[1].address;
+
+const defaults = {
+    shouldSucceed: true,
+    owner: OWNER,
+    operator: OPERATOR,
+    isApproved: true,
+};
+
+export const IS_APPROVED_FOR_ALL_SCENARIOS: DebtTokenScenario.IsApprovedForAllScenario[] = [
+    {
+        description: "operator is approved by owner",
+        ...defaults,
+    },
+    {
+        description: "operator is not approved by owner",
+        ...defaults,
+        isApproved: false,
+    },
+    {
+        description: "operator field is malformed",
+        ...defaults,
+        shouldSucceed: false,
+        operator: "0x123",
+        errorType: "DOES_NOT_CONFORM_TO_SCHEMA",
+        errorMessage: 'instance does not match pattern "^0x[0-9a-fA-F]{40}$"',
+    },
+    {
+        description: "owner field is malformed",
+        ...defaults,
+        shouldSucceed: false,
+        owner: "0x123",
+        errorType: "DOES_NOT_CONFORM_TO_SCHEMA",
+        errorMessage: 'instance does not match pattern "^0x[0-9a-fA-F]{40}$"',
+    },
+];
