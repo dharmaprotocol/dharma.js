59ff754d08c6e30aa26d0490edca9929ffa6c68c
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-10 17:53:20 -0700

    test: define `getApproved` scenarios

--- __test__/integration/debt_token_api/scenarios/index.ts
+++ __test__/integration/debt_token_api/scenarios/index.ts
@@ -4,6 +4,8 @@ import { OWNER_OF_SCENARIOS } from "./owner_of";
 import { EXISTS_SCENARIOS } from "./exists";
 import { SUCCESSFUL_APPROVE_SCENARIOS, UNSUCCESSFUL_APPROVE_SCENARIOS } from "./approve";
 
+export { GET_APPROVED_SCENARIOS } from "./get_approved";
+
 export {
     DebtTokenScenario,
     BALANCE_OF_SCENARIOS,
