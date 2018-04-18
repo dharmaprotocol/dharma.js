6fcdfeadd9f11606768a6ee42d5b6c650e619831
Christopher Min <chris@medisas.com>
2018-04-18 16:52:12 -0700

    fix: use constants when returning term contract type

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,15 +2,15 @@ export const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const NULL_BYTES32 = "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 export namespace ERC20TokenSymbol {
-    export const REP = "REP";
-    export const ZRX = "ZRX";
-    export const MKR = "MKR";
+  export const REP = "REP";
+  export const ZRX = "ZRX";
+  export const MKR = "MKR";
 }
 
 export const NULL_ECDSA_SIGNATURE = {
-    r: "",
-    s: "",
-    v: 0,
+  r: "",
+  s: "",
+  v: 0,
 };
 
 export const WEB3_ERROR_INVALID_ADDRESS = "invalid address";
@@ -26,5 +26,10 @@ export const TOKEN_REGISTRY_CONTRACT_CACHE_KEY = "TokenRegistry";
 export const TOKEN_TRANSFER_PROXY_CONTRACT_CACHE_KEY = "TokenTransferProxy";
 export const TERMS_CONTRACT_REGISTRY_CONTRACT_CACHE_KEY = "TermsContractRegistry";
 export const COLLATERALIZED_SIMPLE_INTEREST_TERMS_CONTRACT_CACHE_KEY =
-    "CollateralizedSimpleInterestTermsContract";
+  "CollateralizedSimpleInterestTermsContract";
 export const COLLATERALIZER_CONTRACT_CACHE_KEY = "CollateralizerContract";
+
+export const TERMS_CONTRACT_TYPES = {
+  COLLATERALIZED_SIMPLE_INTEREST_LOAN: "CollateralizedSimpleInterestLoan",
+  SIMPLE_INTEREST_LOAN: "SimpleInterestLoan",
+};
