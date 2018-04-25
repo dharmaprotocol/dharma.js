72b4dbddcb27f73d1def63ed65311c23448a14fa
Christopher Min <chris@medisas.com>
2018-04-24 19:07:10 -0700

    feat: add scaleDown and scaleUp utils

--- utils/units.ts
+++ utils/units.ts
@@ -1,19 +1,26 @@
-import {BigNumber} from "./bignumber";
+import { BigNumber } from "./bignumber";
 
 const ether = (decimalAmount: number): BigNumber => {
     const decimalBigNumber = new BigNumber(decimalAmount);
     const weiInEther = new BigNumber(10 ** 18);
 
     return decimalBigNumber.times(weiInEther);
-}
+};
 
 const gwei = (decimalAmount: number): BigNumber => {
-    return new BigNumber(decimalAmount * (10 ** 9));
-}
+    return new BigNumber(decimalAmount * 10 ** 9);
+};
 
 const percent = (decimalAmount: number): BigNumber => {
-    return new BigNumber((decimalAmount / 100) * (10 ** 9));
-}
+    return new BigNumber(decimalAmount / 100 * 10 ** 9);
+};
 
+const scaleDown = (baseAmount: BigNumber, exponent: BigNumber): BigNumber => {
+    return baseAmount.div(new BigNumber(10).pow(exponent.toNumber()));
+};
 
-export { ether, gwei, percent };
+const scaleUp = (baseAmount: BigNumber, exponent: BigNumber): BigNumber => {
+    return baseAmount.times(new BigNumber(10).pow(exponent.toNumber()));
+};
+
+export { ether, gwei, percent, scaleDown, scaleUp };
