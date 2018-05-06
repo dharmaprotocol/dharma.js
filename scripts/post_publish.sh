8b826805e3db11750ae97b201be70da8880ffb5f
Nadav Hollander <nadavhollander@users.noreply.github.com>
2018-05-06 16:31:27 -0700

    docs: create pipeline for json docs (#148)

---
+++ scripts/post_publish.sh
@@ -0,0 +1,13 @@
+npm run docs:generate
+
+S3_BASE_URL=s3://json-docs/dharma.js
+
+# Version key/value should be on his own line
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+S3_URL=$S3_BASE_URL/$PACKAGE_VERSION npm run docs:upload
