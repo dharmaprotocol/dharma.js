b0e05f42a07b56853aeceebb6654126f996cb2f1
Chris Min <chris@dharma.io>
2018-06-08 14:37:49 -0700

    refactor: rename to DebtOrderData and DebtOrderDataWrapper

--- src/adapters/adapter.ts
+++ src/adapters/adapter.ts
@@ -1,12 +1,12 @@
-import { DebtOrder, DebtRegistryEntry } from "../types";
+import { DebtOrderData, DebtRegistryEntry } from "../types";
 import { CollateralizedSimpleInterestLoanOrder } from "./collateralized_simple_interest_loan_adapter";
 import { SimpleInterestLoanOrder } from "./simple_interest_loan_adapter";
 
 export interface Adapter {
-    fromDebtOrder: (
-        debtOrder: DebtOrder,
+    fromDebtOrderData: (
+        debtOrder: DebtOrderData,
     ) => Promise<SimpleInterestLoanOrder | CollateralizedSimpleInterestLoanOrder>;
-    toDebtOrder: (params: object) => Promise<DebtOrder>;
+    toDebtOrderData: (params: object) => Promise<DebtOrderData>;
     fromDebtRegistryEntry: (entry: DebtRegistryEntry) => Promise<object>;
     getRepaymentSchedule: (entry: DebtRegistryEntry) => number[];
     unpackParameters: (packedParams: string) => object;
@@ -17,13 +17,13 @@ export interface Adapter {
 
 export function conformsToAdapterInterface(object: any): object is Adapter {
     return (
-        "fromDebtOrder" in object &&
-        "toDebtOrder" in object &&
+        "fromDebtOrderData" in object &&
+        "toDebtOrderData" in object &&
         "fromDebtRegistryEntry" in object &&
         "getRepaymentSchedule" in object &&
         "unpackParameters" in object &&
-        typeof object.fromDebtOrder === "function" &&
-        typeof object.toDebtOrder === "function" &&
+        typeof object.fromDebtOrderData === "function" &&
+        typeof object.toDebtOrderData === "function" &&
         typeof object.fromDebtRegistryEntry === "function" &&
         typeof object.getRepaymentSchedule === "function" &&
         typeof object.unpackParameters === "function"
