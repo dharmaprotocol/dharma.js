44d192a4c58793a29c17ba483da6578c63a8b7bb
Graeme <graemelearn@protonmail.com>
2018-06-12 16:03:21 -0700

    test: add some doc

--- __test__/unit/types/time_interval/scenarios.ts
+++ __test__/unit/types/time_interval/scenarios.ts
@@ -20,42 +20,49 @@ export const timeIntervalScenarios: TimeIntervalScenario[] = [
         ...defaultParams,
         amount: 1,
         unit: "day",
+        // 1528842347 + 60  * 60 * 24 = 1528928747
         blockTimeAfterInterval: new BigNumber(1528928747),
     },
     {
         ...defaultParams,
         amount: 4,
         unit: "days",
+        // 1528842347 + 60  * 60 * 24 * 4 = 1529187947
         blockTimeAfterInterval: new BigNumber(1529187947),
     },
     {
         ...defaultParams,
         amount: 1,
         unit: "week",
+        // 1528842347 + 60  * 60 * 24 * 7 = 1529187947
         blockTimeAfterInterval: new BigNumber(1529447147),
     },
     {
         ...defaultParams,
         amount: 2,
         unit: "weeks",
+        // 1528842347 + 60  * 60 * 24 * 14 = 1530051947
         blockTimeAfterInterval: new BigNumber(1530051947),
     },
     {
         ...defaultParams,
         amount: 1,
         unit: "month",
+        // 1528842347 + 60  * 60 * 24 * 30 = 1531434347
         blockTimeAfterInterval: new BigNumber(1531434347),
     },
     {
         ...defaultParams,
         amount: 3,
         unit: "months",
+        // 1528842347 + 60  * 60 * 24 * 92 = 1536791147
         blockTimeAfterInterval: new BigNumber(1536791147),
     },
     {
         ...defaultParams,
         amount: 1,
         unit: "year",
+        // 1528842347 + 60  * 60 * 24 * 365 = 1560378347
         blockTimeAfterInterval: new BigNumber(1560378347),
     },
     {
