b92d1f6c35fec92b6cb5087a9c078d21625adc44
Graeme <graemelearn@protonmail.com>
2018-06-13 16:10:22 -0700

    test: add tests for isExpired

--- __test__/unit/debt_order/debt_order.spec.ts
+++ __test__/unit/debt_order/debt_order.spec.ts
@@ -4,13 +4,12 @@ import { Dharma } from "../../../src/dharma";
 
 jest.unmock("@dharmaprotocol/contracts");
 
-import { DebtOrder } from "../../../src/debt_order/";
-
-// Types
+import { IS_EXPIRED_SCENARIOS } from "./scenarios/is_expired_scenarios";
 import { DEBT_ORDER_PARAMS_ONE } from "./scenarios/valid_debt_order_params";
 
 // Test runners
 import { testCreate } from "./runners/create";
+import { testExpired } from "./runners/is_expired";
 
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const web3 = new Web3(provider);
@@ -19,5 +18,11 @@ const dharma = new Dharma(web3.currentProvider);
 describe("Debt Order (Integration)", () => {
     describe("#create", async () => {
         await testCreate(dharma, DEBT_ORDER_PARAMS_ONE);
+    });
+
+    describe("#isExpired", async () => {
+        IS_EXPIRED_SCENARIOS.forEach(async (scenario) => {
+            await testExpired(dharma, scenario);
+        });
     });
 });
