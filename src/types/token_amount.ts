f0f2308b268e087c1cb045d879d44393fa65775b
Kayvon Tehranian <ktehranian@gmail.com>
2018-06-13 12:01:49 -0700

    refactor: token amount should offer standard construction

--- src/types/token_amount.ts
+++ src/types/token_amount.ts
@@ -4,48 +4,31 @@ import { BigNumber } from "../../utils/bignumber";
 // Types
 import { Token } from "../types";
 
-export enum TokenAmountType {
-    Raw,
-    Decimal,
-}
-
-export interface TokenAmountParams {
-    symbol: string;
-    amount: BigNumber;
-    type: TokenAmountType;
-}
-
 export class TokenAmount {
+    public static fromRaw(rawAmount: BigNumber, symbol: string) {
+        const token = new Token(symbol);
+        const decimalAmount = TokenAmount.convertToDecimal(rawAmount, token.numDecimals);
+        return new TokenAmount(decimalAmount, symbol);
+    }
+
     private static convertToRaw(decimalAmount: BigNumber, numDecimals: BigNumber): BigNumber {
         return decimalAmount.mul(new BigNumber(10).pow(numDecimals.toNumber()));
     }
 
-    private static convertToDecimal(rawAmount: BigNumber, numDecimals: BigNumber): BigNumber {
-        return rawAmount.div(new BigNumber(10).pow(numDecimals.toNumber()));
+    private static convertToDecimal(rawAmount: BigNumber, numDecimals: BigNumber): number {
+        return rawAmount.div(new BigNumber(10).pow(numDecimals.toNumber())).toNumber();
     }
 
     public readonly rawAmount: BigNumber;
-    private token: Token;
+    private readonly token: Token;
 
-    constructor(params: TokenAmountParams) {
-        this.token = new Token(params.symbol);
-
-        switch (params.type) {
-            case TokenAmountType.Decimal:
-                this.rawAmount = TokenAmount.convertToRaw(params.amount, this.token.numDecimals);
-                break;
-
-            case TokenAmountType.Raw:
-                this.rawAmount = params.amount;
-                break;
-
-            default:
-                throw new Error("invalid token amount type");
-        }
+    constructor(amount: number, symbol: string) {
+        this.token = new Token(symbol);
+        this.rawAmount = TokenAmount.convertToRaw(new BigNumber(amount), this.token.numDecimals);
     }
 
-    get tokenNumDecimals(): BigNumber {
-        return this.token.numDecimals;
+    get tokenNumDecimals(): number {
+        return this.token.numDecimals.toNumber();
     }
 
     get tokenName(): string {
@@ -56,7 +39,7 @@ export class TokenAmount {
         return this.token.symbol;
     }
 
-    get decimalAmount(): BigNumber {
+    get decimalAmount(): number {
         return TokenAmount.convertToDecimal(this.rawAmount, this.token.numDecimals);
     }
 
