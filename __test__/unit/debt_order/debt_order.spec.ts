e5311517720d9f2faceacad86b0900f686045baf
Kayvon Tehranian <ktehranian@gmail.com>
2018-06-13 15:18:33 -0700

    test: make debt order spec an integration test suite

--- __test__/unit/debt_order/debt_order.spec.ts
+++ __test__/unit/debt_order/debt_order.spec.ts
@@ -1,24 +1,25 @@
 // Internal dependencies
+import * as Web3 from "web3";
 import { Dharma } from "../../../src/dharma";
 
-// Mock the dependency
-jest.mock("../../../src/dharma");
+jest.unmock("@dharmaprotocol/contracts");
+
+import { DebtOrder } from "../../../src/debt_order/";
 
 // Types
-import { ValidDebtOrderParams } from "./scenarios/valid_debt_order_params";
+import { DEBT_ORDERS } from "./scenarios/valid_debt_order_params";
 
 // Test runners
-import { CreateRunner } from "./runners/create";
+import { testCreate } from "./runners/create";
 
-// Create a mocked instance of Dharma.
-const dharma = new Dharma({}, {});
+const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+const web3 = new Web3(provider);
+const dharma = new Dharma(web3.currentProvider);
 
-describe("Debt Order (Unit)", () => {
+describe("Debt Order (Integration)", () => {
     describe("#create", () => {
-        const createRunner = new CreateRunner(dharma);
-
-        createRunner.testCreate.bind(this);
-
-        ValidDebtOrderParams.forEach(createRunner.testCreate);
+        DEBT_ORDERS.forEach(async (params, index) => {
+            await testCreate(dharma, params, index);
+        });
     });
 });
