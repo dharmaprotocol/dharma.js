e9a5e021a16879f13584a38d35347a2eafaafaed
Kayvon Tehranian <ktehranian@gmail.com>
2018-06-13 15:11:08 -0700

    fix: use standardized initializers when constructing debt order params

--- __test__/unit/debt_order/scenarios/valid_debt_order_params.ts
+++ __test__/unit/debt_order/scenarios/valid_debt_order_params.ts
@@ -2,18 +2,20 @@
 import { DebtOrderParams } from "../../../../src/debt_order";
 
 // Types
-import { InterestRate, TimeInterval, TokenAmount } from "../../../../src/types";
+import { Address, InterestRate, TimeInterval, TokenAmount } from "../../../../src/types";
 
 // Test utils
 import { ACCOUNTS } from "../../../accounts";
 
 const debtor = ACCOUNTS[1];
 
-export const ValidDebtOrderParams: DebtOrderParams[] = [{
-    principal: new TokenAmount(5, "REP"),
-    collateral: new TokenAmount(10, "WETH"),
-    interestRate: new InterestRate(12.3),
-    termLength: new TimeInterval(6, "months"),
-    debtorAddress: debtor.address,
-    expiresIn: new TimeInterval(5, "days"),
-}];
+export const ValidDebtOrderParams: DebtOrderParams[] = [
+    {
+        principal: new TokenAmount(5, "REP"),
+        collateral: new TokenAmount(10, "WETH"),
+        interestRate: new InterestRate(12.3),
+        termLength: new TimeInterval(6, "months"),
+        debtorAddress: new Address(debtor.address),
+        expiresIn: new TimeInterval(5, "days"),
+    },
+];
