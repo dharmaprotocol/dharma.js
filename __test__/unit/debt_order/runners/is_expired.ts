1f62cb61499a9fe1e1c1a456721af8d856731c74
Graeme <graeme@dharma.io>
2018-06-13 17:13:12 -0700

    test: add tests for isExpired (#204)

---
+++ __test__/unit/debt_order/runners/is_expired.ts
@@ -0,0 +1,23 @@
+// Debt Order
+import { DebtOrder, DebtOrderParams } from "../../../../src/debt_order";
+
+// Import Dharma for typing-checking.
+import { Dharma } from "../../../../src";
+
+import { IsExpiredScenario } from "../scenarios/expired_debt_orders";
+
+export async function testExpired(dharma: Dharma, scenario: IsExpiredScenario) {
+    describe(scenario.description, () => {
+        let debtOrder: DebtOrder;
+
+        beforeAll(async () => {
+            debtOrder = await DebtOrder.create(dharma, (scenario.params));
+        });
+
+        test(`eventually returns ${scenario.isExpired}`, async () => {
+            const isExpired = await debtOrder.isExpired();
+
+            expect(isExpired).toEqual(scenario.isExpired);
+        });
+    });
+}
---
+++ __test__/unit/debt_order/scenarios/is_expired_scenarios.ts
@@ -0,0 +1,55 @@
+// Debt Order
+import { DebtOrderParams } from "../../../../src/debt_order";
+
+// Types
+import { Address, InterestRate, TimeInterval, TokenAmount } from "../../../../src/types";
+
+// Test utils
+import { ACCOUNTS } from "../../../accounts";
+
+const debtor = ACCOUNTS[1];
+
+export interface IsExpiredScenario {
+    description: string;
+    params: DebtOrderParams;
+    isExpired: boolean;
+}
+
+export const IS_EXPIRED_SCENARIOS: IsExpiredScenario[] = [
+    {
+        description: "when given valid params and the expiration is 5 days in the future",
+        params: {
+                principal: new TokenAmount(5, "REP"),
+                collateral: new TokenAmount(10, "WETH"),
+                interestRate: new InterestRate(12.3),
+                termLength: new TimeInterval(6, "months"),
+                debtorAddress: new Address(debtor.address),
+                expiresIn: new TimeInterval(5, "days"),
+        },
+        isExpired: false,
+    },
+    {
+        description: "when given valid params and the expiration is 5 days in the past",
+        params: {
+            principal: new TokenAmount(5, "REP"),
+            collateral: new TokenAmount(10, "WETH"),
+            interestRate: new InterestRate(12.3),
+            termLength: new TimeInterval(6, "months"),
+            debtorAddress: new Address(debtor.address),
+            expiresIn: new TimeInterval(-5, "days"),
+        },
+        isExpired: true,
+    },
+    {
+        description: "when given valid params and the expiration is 1 day in the past",
+        params: {
+            principal: new TokenAmount(5, "REP"),
+            collateral: new TokenAmount(10, "WETH"),
+            interestRate: new InterestRate(12.3),
+            termLength: new TimeInterval(6, "months"),
+            debtorAddress: new Address(debtor.address),
+            expiresIn: new TimeInterval(-1, "day"),
+        },
+        isExpired: true,
+    },
+];
