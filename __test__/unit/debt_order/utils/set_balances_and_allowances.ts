6e98e14a0f39de39a607b1c62529318915f33b75
Graeme <graemelearn@protonmail.com>
2018-06-14 18:03:35 -0700

    style: fix whitespace

--- __test__/unit/debt_order/utils/set_balances_and_allowances.ts
+++ __test__/unit/debt_order/utils/set_balances_and_allowances.ts
@@ -17,7 +17,12 @@ import { ContractsAPI, TokenAPI } from "../../../../src/apis";
 
 const TX_DEFAULTS = { from: ACCOUNTS[0].address, gas: 400000 };
 
-export const setBalancesAndAllowances = async (dharma: Dharma, web3: Web3, params: DebtOrderParams, creditorAddress: string) => {
+export const setBalancesAndAllowances = async (
+    dharma: Dharma,
+    web3: Web3,
+    params: DebtOrderParams,
+    creditorAddress: string,
+) => {
     const debtorAddress = params.debtorAddress.toString();
     const contractOwner = ACCOUNTS[0];
 
@@ -25,13 +30,9 @@ export const setBalancesAndAllowances = async (dharma: Dharma, web3: Web3, param
     const tokenApi = new TokenAPI(web3, contractsApi);
 
     const tokenRegistry = await dharma.contracts.loadTokenRegistry();
-    const principalTokenAddress = await tokenRegistry.getTokenAddressBySymbol.callAsync(
-        "REP",
-    );
+    const principalTokenAddress = await tokenRegistry.getTokenAddressBySymbol.callAsync("REP");
 
-    const collateralTokenAddress = await tokenRegistry.getTokenAddressBySymbol.callAsync(
-        "MKR",
-    );
+    const collateralTokenAddress = await tokenRegistry.getTokenAddressBySymbol.callAsync("MKR");
 
     const principalToken = await DummyTokenContract.at(principalTokenAddress, web3, TX_DEFAULTS);
     const collateralToken = await DummyTokenContract.at(collateralTokenAddress, web3, TX_DEFAULTS);
@@ -39,15 +40,19 @@ export const setBalancesAndAllowances = async (dharma: Dharma, web3: Web3, param
     // Grant creditor a balance of tokens
     await principalToken.setBalance.sendTransactionAsync(
         creditorAddress,
-        params.principal.rawAmount, {
+        params.principal.rawAmount,
+        {
             from: contractOwner.address,
-        });
+        },
+    );
 
     await collateralToken.setBalance.sendTransactionAsync(
         debtorAddress,
-        params.collateral.rawAmount, {
+        params.collateral.rawAmount,
+        {
             from: contractOwner.address,
-        });
+        },
+    );
 
     await tokenApi.setUnlimitedProxyAllowanceAsync(principalToken.address, {
         from: creditorAddress,
