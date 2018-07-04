c874dca9624bddbbfd46279b6e2e20f35f628f6c
Chris Min <chris@dharma.io>
2018-07-03 18:23:27 -0700

    fix: fix makeRepayment

--- src/debt_order/index.ts
+++ src/debt_order/index.ts
@@ -350,21 +350,25 @@ export class DebtOrder {
     public async makeRepayment(repaymentAmount?: number): Promise<string> {
         const agreementId = this.getAgreementId();
         const tokenSymbol = this.params.principal.tokenSymbol;
-
-        const repaymentAmountType = new TokenAmount(repaymentAmount, tokenSymbol);
-
         const principalTokenAddressString = await this.dharma.contracts.getTokenAddressBySymbolAsync(
             tokenSymbol,
         );
 
         // If repaymentAmount is not specified, we default to the expected amount per installment.
-        const rawRepaymentAmount =
-            repaymentAmountType.rawAmount ||
-            (await this.dharma.servicing.getExpectedAmountPerRepayment(agreementId));
+        let rawRepaymentAmount: BigNumber;
+
+        if (repaymentAmount) {
+            const repaymentAmountType = new TokenAmount(repaymentAmount, tokenSymbol);
+            rawRepaymentAmount = repaymentAmountType.rawAmount;
+        } else {
+            rawRepaymentAmount = await this.dharma.servicing.getExpectedAmountPerRepayment(
+                agreementId,
+            );
+        }
 
         return this.dharma.servicing.makeRepayment(
             agreementId,
-            new BigNumber(rawRepaymentAmount),
+            rawRepaymentAmount,
             principalTokenAddressString,
         );
     }
