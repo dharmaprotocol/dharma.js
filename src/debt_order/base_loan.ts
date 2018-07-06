8ff8d9e77c641378ff0247a7eae6f144b536d599
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-06 11:21:19 -0700

    feat: abstract base class exposes methods as public

--- src/debt_order/base_loan.ts
+++ src/debt_order/base_loan.ts
@@ -8,7 +8,7 @@ import { Dharma } from "../dharma";
 
 const SALT_DECIMALS = 20;
 
-interface DebtOrderConstructorParams {
+export interface BaseLoanConstructorParams {
     principal: TokenAmount;
     collateral: TokenAmount;
     interestRate: InterestRate;
@@ -17,18 +17,18 @@ interface DebtOrderConstructorParams {
     expiresAt: number;
 }
 
-export class BaseLoan {
-    private static generateSalt(): BigNumber {
+export abstract class BaseLoan {
+    public static generateSalt(): BigNumber {
         return BigNumber.random(SALT_DECIMALS).times(new BigNumber(10).pow(SALT_DECIMALS));
     }
 
-    private constructor(
-        private dharma: Dharma,
-        private params: DebtOrderConstructorParams,
-        private data: DebtOrderData,
+    constructor(
+        public dharma: Dharma,
+        public params: BaseLoanConstructorParams,
+        public data: DebtOrderData,
     ) {}
 
-    private async enableTokenTransfers(
+    public async enableTokenTransfers(
         address: EthereumAddress,
         tokenSymbol: string,
     ): Promise<string> {
@@ -39,19 +39,19 @@ export class BaseLoan {
         });
     }
 
-    private getAgreementId(): string {
+    public getAgreementId(): string {
         return new DebtOrderDataWrapper(this.data).getIssuanceCommitmentHash();
     }
 
-    private serialize(): DebtOrderData {
+    public serialize(): DebtOrderData {
         return this.data;
     }
 
-    private async getCurrentBlocktime(): Promise<number> {
+    public async getCurrentBlocktime(): Promise<number> {
         return this.dharma.blockchain.getCurrentBlockTime();
     }
 
-    private async getCurrentUser(): Promise<string> {
+    public async getCurrentUser(): Promise<string> {
         const accounts = await this.dharma.blockchain.getAccounts();
 
         return accounts[0];
