f1692feaf43b8a6e5733b1a8de8abb27ae0b00b9
Chris Min <chris@dharma.io>
2018-07-11 18:12:59 -0700

    fix: add blockchain snapshotting to LoanRequest#generateLoan tests

--- __test__/integration/loan/runners/generate_loan.ts
+++ __test__/integration/loan/runners/generate_loan.ts
@@ -10,13 +10,26 @@ import { Dharma } from "../../../../src/dharma";
 import { ACCOUNTS } from "../../../accounts";
 
 // Utils
+import { Web3Utils } from "../../../../utils/web3_utils";
 import { setBalancesAndAllowances } from "../utils/set_balances_and_allowances";
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+const web3 = new Web3(provider);
+const web3Utils = new Web3Utils(web3);
+
 const CREDITOR = ACCOUNTS[3];
 
 export async function testGenerateLoan(dharma: Dharma, params: LoanRequestParams) {
+    let snapshotId: number;
     let loanRequest: LoanRequest;
+
+    beforeEach(async () => {
+        snapshotId = await web3Utils.saveTestSnapshot();
+    });
+
+    afterEach(async () => {
+        await web3Utils.revertToSnapshot(snapshotId);
+    });
 
     describe("when the LoanRequest has been filled", () => {
         beforeAll(async () => {
