41873a140281a0253c1efc3c2fae8c55a100fd93
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-17 16:46:19 -0700

    test: add'l unit test for getTerms when LoanRequest is loaded

--- __test__/integration/loan/runners/get_terms.ts
+++ __test__/integration/loan/runners/get_terms.ts
@@ -8,6 +8,23 @@ import { Dharma } from "../../../../src/dharma";
 export async function testGetTerms(dharma: Dharma, params: LoanRequestParams) {
     let loanRequest: LoanRequest;
 
+    function generateExpectation() {
+        return _.chain(params)
+            .omit(["expiresInDuration", "expiresInUnit"])
+            .assign({ expiresAt: loanRequest.params.expiresAt })
+            .mapKeys((value, key) => {
+                switch (key) {
+                    case "collateralToken":
+                        return "collateralTokenSymbol";
+                    case "principalToken":
+                        return "principalTokenSymbol";
+                    default:
+                        return key;
+                }
+            })
+            .value();
+    }
+
     describe("when a loan request is originally created", () => {
         beforeAll(async () => {
             loanRequest = await LoanRequest.create(dharma, params);
@@ -16,22 +33,21 @@ export async function testGetTerms(dharma: Dharma, params: LoanRequestParams) {
         test("returns the loan request's terms", async () => {
             const terms = loanRequest.getTerms();
 
-            const expectation = _.chain(params)
-                .omit(["expiresInDuration", "expiresInUnit"])
-                .assign({ expiresAt: loanRequest.params.expiresAt })
-                .mapKeys((value, key) => {
-                    switch (key) {
-                        case "collateralToken":
-                            return "collateralTokenSymbol";
-                        case "principalToken":
-                            return "principalTokenSymbol";
-                        default:
-                            return key;
-                    }
-                })
-                .value();
+            expect(terms).toEqual(generateExpectation());
+        });
+    });
 
-            expect(terms).toEqual(expectation);
+    describe("when a loan request is loaded from serialized data", () => {
+        beforeAll(async () => {
+            const tempLoanRequest = await LoanRequest.create(dharma, params);
+            const data = tempLoanRequest.serialize();
+            loanRequest = await LoanRequest.load(dharma, data);
+        });
+
+        test("returns the loan request's terms", async () => {
+            const terms = loanRequest.getTerms();
+
+            expect(terms).toEqual(generateExpectation());
         });
     });
 }
