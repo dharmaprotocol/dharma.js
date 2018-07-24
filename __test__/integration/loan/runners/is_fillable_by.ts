b905a9c4f607c461e47c77d39f58db23db5da775
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-24 13:19:52 -0700

    refactor: create and use wrapper function to set token balances

--- __test__/integration/loan/runners/is_fillable_by.ts
+++ __test__/integration/loan/runners/is_fillable_by.ts
@@ -2,12 +2,12 @@ import * as Web3 from "web3";
 
 import { Dharma } from "../../../../src/dharma";
 import { LoanRequest, LoanRequestParams } from "../../../../src/loan";
+import { TokenAmount } from "../../../../src/types";
+import { DummyTokenContract } from "../../../../src/wrappers";
 
 import { BigNumber } from "../../../../utils/bignumber";
 import { Web3Utils } from "../../../../utils/web3_utils";
 import { ACCOUNTS } from "../../../accounts";
-
-import { DummyTokenContract } from "../../../../src/wrappers";
 
 const CREDITOR = ACCOUNTS[5].address;
 
@@ -17,10 +17,26 @@ const web3Utils = new Web3Utils(web3);
 
 const TX_DEFAULTS = { from: ACCOUNTS[0].address, gas: 4712388 };
 
+async function setBalance(dharma: Dharma, amount: TokenAmount, recipient: string): Promise<string> {
+    const tokenRegistry = await dharma.contracts.loadTokenRegistry();
+
+    const tokenAddress = await tokenRegistry.getTokenAddressBySymbol.callAsync(amount.tokenSymbol);
+
+    const token = await DummyTokenContract.at(tokenAddress, web3, TX_DEFAULTS);
+
+    return token.setBalance.sendTransactionAsync(recipient, amount.rawAmount);
+}
+
 export async function testIsFillableBy(dharma: Dharma, params: LoanRequestParams) {
     describe("for a loan request with valid parameters", () => {
         let currentSnapshotId: number;
         let loanRequest: LoanRequest;
+
+        beforeAll(async () => {
+            const amount = new TokenAmount(params.collateralAmount, params.collateralToken);
+
+            await setBalance(dharma, amount, params.debtorAddress);
+        });
 
         beforeEach(async () => {
             currentSnapshotId = await web3Utils.saveTestSnapshot();
@@ -33,25 +49,6 @@ export async function testIsFillableBy(dharma: Dharma, params: LoanRequestParams
         });
 
         describe("when the debtor has insufficient balance", () => {
-            beforeEach(async () => {
-                const tokenRegistry = await dharma.contracts.loadTokenRegistry();
-
-                const collateralTokenAddress = await tokenRegistry.getTokenAddressBySymbol.callAsync(
-                    params.collateralToken,
-                );
-
-                const collateralToken = await DummyTokenContract.at(
-                    collateralTokenAddress,
-                    web3,
-                    TX_DEFAULTS,
-                );
-
-                await collateralToken.setBalance.sendTransactionAsync(
-                    params.debtorAddress,
-                    new BigNumber(0),
-                );
-            });
-
             test("eventually returns false", async () => {
                 await loanRequest.allowCollateralTransfer();
                 const isFillable = await loanRequest.isFillableBy(CREDITOR);
