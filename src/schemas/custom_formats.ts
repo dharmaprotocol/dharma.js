2d550df12e3e2bb165814779ef1250d8e0a16420
Graeme <graemelearn@protonmail.com>
2018-07-25 11:21:05 -0700

    feat: add method for checking if a function conforms to BigNumber

--- src/schemas/custom_formats.ts
+++ src/schemas/custom_formats.ts
@@ -3,13 +3,13 @@ import { BigNumber } from "../../utils/bignumber";
 // is required by the schema but undefined, the validator will throw an error at another point.)
 // Hence, we can skip validating format if the input is undefined.
 
-export const bigNumberFormat = function(input) {
+export const bigNumberFormat = (input) => {
     const regex = RegExp("^\\d+(\\.\\d+)?$");
 
     return input === undefined || (isBigNumber(input) && regex.test(input.toString()));
 };
 
-export const wholeBigNumberFormat = function(input) {
+export const wholeBigNumberFormat = (input) => {
     const regex = RegExp("^\\d+$");
 
     return input === undefined || (isBigNumber(input) && regex.test(input.toString()));
@@ -19,7 +19,7 @@ export const wholeBigNumberFormat = function(input) {
  * NOTE: BigNumber.js is currently changing their API, and the method `isBigNumber` on instances
  * of BigNumber is deprecated. I.E. this will not be available in the next version (v6.0.)
  *
- * However, Truffe.js uses v5.0, and so we are dependent on v5.0 until Truffle upgrades.
+ * However, Truffle.js uses v5.0, and so we are dependent on v5.0 until Truffle upgrades.
  *
  * In version 6.0, this can be replaced with `BigNumber.isBigNumber(object);`
  *
@@ -28,7 +28,10 @@ export const wholeBigNumberFormat = function(input) {
  */
 function isBigNumber(object: any): boolean {
     return (
+        // Note: when minified, properties such as isBigNumber can get renamed ("mangled"),
+        // and so we add a function to our BigNumber wrapper called "conformsToBigNumber".
         object.isBigNumber ||
+        (object.conformsToBigNumber === "Function" && object.conformsToBigNumber()) ||
         object instanceof BigNumber ||
         (object.constructor && object.constructor.name === "BigNumber")
     );
