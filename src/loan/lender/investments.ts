40b4bde4d8aa8d7172643c4849350f0c1bd690fb
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-31 10:44:46 -0700

    refactor: remove duplicate code from investments

--- src/loan/lender/investments.ts
+++ src/loan/lender/investments.ts
@@ -2,10 +2,6 @@ import { Investment } from "./investment";
 
 import { Dharma } from "../../dharma";
 
-import { EthereumAddress, InterestRate, TimeInterval, TokenAmount } from "../../types";
-
-import { BaseLoanConstructorParams } from "../agreement";
-
 /**
  * Describes a collection of investments and provides functionality for managing such a collection.
  */
@@ -20,33 +16,7 @@ export class Investments {
 
         return Promise.all(
             agreementIds.map(async (agreementId) => {
-                const debtOrderData = await dharma.order.getDebtOrder(agreementId);
-
-                const parameters = dharma.adapters.collateralizedSimpleInterestLoan.unpackParameters(
-                    debtOrderData.termsContractParameters,
-                );
-
-                const principalSymbol = await dharma.token.getTokenSymbolByIndexAsync(
-                    parameters.principalTokenIndex,
-                );
-
-                const collateralSymbol = await dharma.token.getTokenSymbolByIndexAsync(
-                    parameters.collateralTokenIndex,
-                );
-
-                const params: BaseLoanConstructorParams = {
-                    principal: TokenAmount.fromRaw(parameters.principalAmount, principalSymbol),
-                    collateral: TokenAmount.fromRaw(parameters.collateralAmount, collateralSymbol),
-                    interestRate: new InterestRate(parameters.interestRate.toNumber()),
-                    termLength: new TimeInterval(
-                        parameters.termLength.toNumber(),
-                        parameters.amortizationUnit,
-                    ),
-                    debtorAddress: new EthereumAddress(debtOrderData.debtor),
-                    expiresAt: debtOrderData.expirationTimestampInSec.toNumber(),
-                };
-
-                return new Investment(dharma, params, debtOrderData);
+                return Investment.fetch(dharma, agreementId);
             }),
         );
     }
