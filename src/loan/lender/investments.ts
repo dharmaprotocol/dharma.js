0e2c8e153614193193e1435ee8a4b092e4e41697
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-30 19:09:50 -0700

    feat: make get method on Investments static

--- src/loan/lender/investments.ts
+++ src/loan/lender/investments.ts
@@ -2,52 +2,35 @@ import { Investment } from "./investment";
 
 import { Dharma } from "../../dharma";
 
-import {
-    EthereumAddress,
-    InterestRate,
-    TimeInterval,
-    TokenAmount,
-} from "../../types";
-
-import { Loan } from "../loan";
-
-import { CollateralizedSimpleInterestLoanAdapter } from "../../adapters";
+import { EthereumAddress, InterestRate, TimeInterval, TokenAmount } from "../../types";
 
 import { BaseLoanConstructorParams } from "../agreement";
 
 /**
- * Describes a collection of loans and provides functionality for managing such a collection.
+ * Describes a collection of investments and provides functionality for managing such a collection.
  */
 export class Investments {
-    private readonly owner: EthereumAddress;
-
-    constructor(private readonly dharma: Dharma, owner: string) {
-        this.owner = new EthereumAddress(owner);
-    }
-
     /**
-     * Gets a collection of investments that belong to the current owner.
+     * Retrieves a collection of investments that belong to the specified owner.
      *
-     * @returns {Promise<Loan[]>}
+     * @returns {Promise<Investment[]>}
      */
-    public async get(): Promise<Investment[]> {
-        const agreementIds = await this.dharma.servicing.getDebtsAsync(this.owner.toString());
+    public static async get(dharma: Dharma, owner: string): Promise<Investment[]> {
+        const agreementIds = await dharma.servicing.getInvestmentsAsync(owner);
 
         return Promise.all(
             agreementIds.map(async (agreementId) => {
-                const debtOrderData = await this.dharma.order.getDebtOrder(agreementId);
-                const termsContract = debtOrderData.termsContract;
+                const debtOrderData = await dharma.order.getDebtOrder(agreementId);
 
-                const adapter = await this.dharma.servicing.adapterForTermsContract(
-                    termsContract,
-                ) as CollateralizedSimpleInterestLoanAdapter;
+                const parameters = dharma.adapters.collateralizedSimpleInterestLoan.unpackParameters(
+                    debtOrderData.termsContractParameters,
+                );
 
-                const parameters = await adapter.unpackParameters(debtOrderData.termsContractParameters);
-
-                const principalSymbol = await this.dharma.token.getTokenSymbolByIndexAsync(
+                const principalSymbol = await dharma.token.getTokenSymbolByIndexAsync(
                     parameters.principalTokenIndex,
                 );
-                const collateralSymbol = await this.dharma.token.getTokenSymbolByIndexAsync(
+
+                const collateralSymbol = await dharma.token.getTokenSymbolByIndexAsync(
                     parameters.collateralTokenIndex,
                 );
 
@@ -55,12 +38,15 @@ export class Investments {
                     principal: TokenAmount.fromRaw(parameters.principalAmount, principalSymbol),
                     collateral: TokenAmount.fromRaw(parameters.collateralAmount, collateralSymbol),
                     interestRate: new InterestRate(parameters.interestRate.toNumber()),
-                    termLength: new TimeInterval(parameters.termLength.toNumber(), parameters.amortizationUnit),
+                    termLength: new TimeInterval(
+                        parameters.termLength.toNumber(),
+                        parameters.amortizationUnit,
+                    ),
                     debtorAddress: new EthereumAddress(debtOrderData.debtor),
                     expiresAt: debtOrderData.expirationTimestampInSec.toNumber(),
                 };
 
-                return new Investment(this.dharma, params, debtOrderData);
+                return new Investment(dharma, params, debtOrderData);
             }),
         );
     }
