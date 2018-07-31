4d7772a565f0f6b7dda8c615e83d2be2bd66bd27
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-30 19:09:58 -0700

    test: add spec for investments

---
+++ __test__/integration/loan/investments.spec.ts
@@ -0,0 +1,34 @@
+import { ACCOUNTS } from "../../accounts";
+
+import { Dharma } from "../../../src/dharma";
+
+jest.unmock("@dharmaprotocol/contracts");
+
+import { Investments } from "../../../src/loan/lender/investments";
+
+import { Loan, LoanRequest } from "../../../src/loan/";
+
+import { DEBT_ORDER_PARAMS_ONE } from "./scenarios/valid_debt_order_params";
+
+import { setBalancesAndAllowances } from "./utils/set_balances_and_allowances";
+
+const dharma = new Dharma("http://localhost:8545");
+
+const OWNER = ACCOUNTS[9].address;
+
+describe("Investments (Integration)", () => {
+    describe("#get", () => {
+        describe("querying for an owner with 3 investments", () => {
+            beforeAll(async () => {
+                await setBalancesAndAllowances(dharma, DEBT_ORDER_PARAMS_ONE, OWNER);
+                const loanRequest = await LoanRequest.create(dharma, DEBT_ORDER_PARAMS_ONE);
+                await loanRequest.fill(OWNER);
+            });
+
+            test("returns the correct number of investments ", async () => {
+                const investments = await Investments.get(dharma, OWNER);
+                expect(investments.length).toEqual(1);
+            });
+        });
+    });
+});
