7008b0e45d16562b4b614e7d7892342377c32c97
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-31 10:55:01 -0700

    feat: reset blockchain state after each time we dirty it

--- __test__/integration/loan/investments.spec.ts
+++ __test__/integration/loan/investments.spec.ts
@@ -1,3 +1,5 @@
+import * as Web3 from "web3";
+
 import { ACCOUNTS } from "../../accounts";
 
 import { Dharma } from "../../../src/dharma";
@@ -12,11 +14,27 @@ import { DEBT_ORDER_PARAMS_ONE } from "./scenarios/valid_debt_order_params";
 
 import { setBalancesAndAllowances } from "./utils/set_balances_and_allowances";
 
-const dharma = new Dharma("http://localhost:8545");
+import { Web3Utils } from "../../../utils/web3_utils";
 
-const OWNER = ACCOUNTS[7].address;
+const host = "http://localhost:8545";
+const dharma = new Dharma(host);
+const provider = new Web3.providers.HttpProvider(host);
+const web3 = new Web3(provider);
+const web3Utils = new Web3Utils(web3);
+
+const OWNER = ACCOUNTS[8].address;
 
 describe("Investments (Integration)", () => {
+    let currentSnapshotId: number;
+
+    beforeAll(async () => {
+        currentSnapshotId = await web3Utils.saveTestSnapshot();
+    });
+
+    afterEach(async () => {
+        await web3Utils.revertToSnapshot(currentSnapshotId);
+    });
+
     describe("#get", () => {
         describe("querying for an owner with 1 investment", () => {
             beforeAll(async () => {
