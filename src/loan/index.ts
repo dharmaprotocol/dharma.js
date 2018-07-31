5ca9b8e4def513a23a7d2c0ceaddf202d3480f76
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-31 14:34:21 -0700

    feat: export Investment, Investments and Allowance from loan dir

--- src/loan/index.ts
+++ src/loan/index.ts
@@ -1,2 +1,5 @@
 export { LoanRequest, LoanRequestParams } from "./loan_request";
 export { Loan } from "./loan";
+export { Investment } from "./Investment";
+export { Investments } from "./Investments";
+export { Allowance } from "./Allowance";
