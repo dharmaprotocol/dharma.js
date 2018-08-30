e69211e65c0144335a813cc52eeacfa5a798804b
Chris Min <chris@dharma.io>
2018-08-30 12:04:29 -0700

    feat: allow user to instantiate Dharma.js with web3 or blockchainHost

--- __test__/unit/dharma.spec.ts
+++ __test__/unit/dharma.spec.ts
@@ -4,38 +4,56 @@ import { Dharma } from "../../src/dharma";
 
 describe("Dharma (Unit)", () => {
     describe("instantiation", () => {
-        describe("when web3 instance of the correct version is passed in", () => {
-            test("successfully instantiates Dharma.js", () => {
-                const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        describe("when web3 is passed in", () => {
+            describe("when web3 instance of the correct version is passed in", () => {
+                test("successfully instantiates Dharma.js", () => {
+                    const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-                const dharma = new Dharma(web3);
+                    const dharma = new Dharma(web3);
 
-                expect(dharma).toBeInstanceOf(Dharma);
-                expect(dharma.web3).toBe(web3);
+                    expect(dharma).toBeInstanceOf(Dharma);
+                    expect(dharma.web3).toBe(web3);
+                });
+            });
+
+            describe("when web3 instance of an incorrect version is passed in with the 1.x.x structure", () => {
+                test("throws", () => {
+                    const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+                    web3.version = "1.0.0";
+
+                    expect(() => new Dharma(web3)).toThrow();
+                });
+            });
+
+            describe("when web3 instance of an incorrect version is passed in with the 0.x.x structure", () => {
+                test("throws", () => {
+                    const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+                    web3.version.api = `${web3.version.api}0`;
+
+                    expect(() => new Dharma(web3)).toThrow();
+                });
             });
         });
 
-        describe("when web3 instance of an incorrect version is passed in with the 1.x.x structure", () => {
-            test("throws", () => {
-                const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        describe("when blockchain host is passed in", () => {
+            describe("when correctly formatted blockchain host is passed in", () => {
+                test("successfully instantiates Dharma.js", () => {
+                    const dharma = new Dharma("http://localhost:8545");
 
-                web3.version = "1.0.0";
+                    expect(dharma).toBeInstanceOf(Dharma);
+                });
+            });
 
-                expect(() => new Dharma(web3)).toThrow();
+            describe("when incorrectly formatted blockchain host is passed in", () => {
+                test("throws", () => {
+                    expect(() => new Dharma("a random string")).toThrow();
+                });
             });
         });
 
-        describe("when web3 instance of an incorrect version is passed in with the 0.x.x structure", () => {
-            test("throws", () => {
-                const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-                web3.version.api = `${web3.version.api}0`;
-
-                expect(() => new Dharma(web3)).toThrow();
-            });
-        });
-
-        describe("when no web3 instance is passed in", () => {
+        describe("when no parameter is passed in", () => {
             describe("when a web3 instance is present on the window", () => {
                 let web3: Web3;
 
--- src/dharma.ts
+++ src/dharma.ts
@@ -25,41 +25,35 @@ class Dharma {
     public blockchain: BlockchainAPI;
     public logs: LogsAPI;
 
-    public readonly web3: Web3;
+    // tslint:disable-next-line
+    private _web3: Web3;
 
-    constructor(web3?: Web3, addressBook: DharmaTypes.AddressBook = {}) {
+    constructor(blockchainInformation?: Web3 | string, addressBook: DharmaTypes.AddressBook = {}) {
         /**
-         * There are two ways we can access a web3 provider:
-         * 1. We pass in web3
-         * 2. Web3 has been injected into the browser window (e.g. via Metamask.)
+         * There are three ways we can access a Web3 instance:
+         * 1. We pass in Web3
+         * 2. We pass in the blockchain host
+         * 3. Web3 has been injected into the browser window (e.g. via Metamask.)
          */
-        if (web3) {
-            // TODO: figure out a cleaner way to get the web3 version used by Dharma
-            const temporaryWeb3 = new Web3();
 
-            const dharmaWeb3Version = temporaryWeb3.version.api;
-
-            // A hacky way of detecting web3 version 1.0.0, as it has breaking API changes
-            if (
-                (typeof web3.version === "string" &&
-                    web3.version.match(/^(\d+)\.?(\d+)\.?(\*|\d+)$/)[0] === "1") ||
-                web3.version.api !== dharmaWeb3Version
-            ) {
-                throw new Error(
-                    `Dharma.js must be instantiated with a Web3 instance of version ${dharmaWeb3Version}`,
-                );
+        if (blockchainInformation) {
+            if (blockchainInformation instanceof Web3) {
+                this.setWeb3WithWeb3Instance(blockchainInformation);
+            } else if (typeof blockchainInformation === "string") {
+                this.setWeb3WithBlockchainHost(blockchainInformation);
             }
-
-            this.web3 = web3;
         } else if (
             typeof (window as any) !== "undefined" &&
             typeof (window as any).web3 !== "undefined"
         ) {
-            // If web3 is available via the browser window, instantiate web3 via the current provider.
-            this.web3 = new Web3((window as any).web3.currentProvider);
+            // If Web3 is available via the browser window, instantiate Web3 via the current provider.
+            this._web3 = new Web3((window as any).web3.currentProvider);
         } else {
             // Otherwise throw...
-            throw new Error("Web3 not specified as a parameter and not found on the window.");
+            throw new Error(
+                "Neither a blockchain host nor a Web3 instance was specified as a parameter," +
+                    " and Web3 was not found on the window.",
+            );
         }
 
         this.contracts = new ContractsAPI(this.web3, addressBook);
@@ -71,6 +65,44 @@ class Dharma {
         this.token = new TokenAPI(this.web3, this.contracts);
         this.blockchain = new BlockchainAPI(this.web3, this.contracts);
         this.logs = new LogsAPI(this.web3, this.contracts);
+    }
+
+    get web3(): Web3 {
+        return this._web3;
+    }
+
+    private setWeb3WithWeb3Instance(web3: Web3) {
+        // TODO: figure out a cleaner way to get the Web3 version used by Dharma
+        const temporaryWeb3 = new Web3();
+
+        const dharmaWeb3Version = temporaryWeb3.version.api;
+
+        // A hacky way of detecting Web3 version 1.0.0, as it has breaking API changes
+        if (
+            (typeof web3.version === "string" &&
+                web3.version.match(/^(\d+)\.?(\d+)\.?(\*|\d+)$/)[0] === "1") ||
+            web3.version.api !== dharmaWeb3Version
+        ) {
+            throw new Error(
+                `Dharma.js must be instantiated with a Web3 instance of version ${dharmaWeb3Version}`,
+            );
+        }
+
+        this._web3 = web3;
+    }
+
+    private setWeb3WithBlockchainHost(blockchainHost: string) {
+        const web3Provider = new Web3.providers.HttpProvider(blockchainHost);
+        const web3 = new Web3(web3Provider);
+
+        // Verify that the node is reachable
+        web3.version.getNode((error, result) => {
+            if (error) {
+                throw error;
+            }
+        });
+
+        this._web3 = web3;
     }
 }
 
