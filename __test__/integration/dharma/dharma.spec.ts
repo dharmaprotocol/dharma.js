47de6df65e8944442d14daade0a8f4f20b1d3177
Graeme <graemelearn@protonmail.com>
2018-09-06 13:53:38 -0700

    build: remove web3-provider-engine

--- __test__/integration/dharma/dharma.spec.ts
+++ __test__/integration/dharma/dharma.spec.ts
@@ -9,15 +9,15 @@ const NETWORK_URL = "http://localhost:8545";
 
 describe("Dharma Instantiation (Integration)", () => {
     describe("with basic web3 provider", () => {
-       it("instantiates successfully", async () => {
-           const provider = new Web3.providers.HttpProvider(NETWORK_URL);
+        it("instantiates successfully", async () => {
+            const provider = new Web3.providers.HttpProvider(NETWORK_URL);
 
-           const dharma = new Dharma(provider);
+            const dharma = new Dharma(provider);
 
-           // We can make a basic blockchain request.
-           const currentAccount = await dharma.blockchain.getCurrentAccount();
+            // We can make a basic blockchain request.
+            const currentAccount = await dharma.blockchain.getCurrentAccount();
 
-           expect(currentAccount.length).toEqual(42);
-       });
+            expect(currentAccount.length).toEqual(42);
+        });
     });
 });
