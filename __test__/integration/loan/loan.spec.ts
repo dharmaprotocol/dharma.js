581f4a6d1c8904b634b67e7ffa3c1f005f8c7288
Graeme <graemelearn@protonmail.com>
2018-09-06 15:43:37 -0700

    fix: instantiation of web3 provider in a test example

--- __test__/integration/loan/loan.spec.ts
+++ __test__/integration/loan/loan.spec.ts
@@ -8,7 +8,7 @@ import { VALID_LOAN_REQUEST } from "./scenarios/valid_loan_request";
 // Test runners
 import { testGetTotalExpectedRepaymentAmount } from "./runners/get_total_expected_repayment_amount";
 
-const dharma = new Dharma(new Web3.Provider("http://localhost:8545"));
+const dharma = new Dharma(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 describe("Loan (Integration)", () => {
     describe("#getTotalExpectedRepaymentAmount", async () => {
