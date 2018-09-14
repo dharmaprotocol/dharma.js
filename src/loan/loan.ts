f106f358134c698827ccaae95a8726426fe763e4
Kayvon Tehranian <ktehranian@gmail.com>
2018-09-13 22:24:45 -0700

    feat: make Loan a new base class for Debt/Investment

--- src/loan/loan.ts
+++ src/loan/loan.ts
@@ -2,7 +2,136 @@ import { DebtOrderData, EthereumAddress, InterestRate, TimeInterval, TokenAmount
 
 import { Dharma } from "../types/dharma";
 
+export interface LoanParams {
+    id: string;
+    principal: TokenAmount;
+    collateral: TokenAmount;
+    interestRate: InterestRate;
+    termLength: TimeInterval;
+    creditor: EthereumAddress;
+    debtor: EthereumAddress;
+}
+
+export interface LoanData {
+    id: string;
+    principalAmount: number;
+    principalTokenSymbol: string;
+    collateralAmount: number;
+    collateralTokenSymbol: string;
+    interestRate: number;
+    termDuration: number;
+    termUnit: string;
+    debtorAddress: string;
+    creditorAddress: string;
+}
+
+export interface ExpandedLoanData extends LoanData {
+    repaidAmount: number;
+    totalExpectedRepaymentAmount: number;
+}
+
+/**
+ * Describes a loan from a lender's perspective, includes functionality for seizing
+ * collateral.
+ */
 export class Loan {
+    public static async fetch<T extends Loan>(dharma: Dharma, id: string): Promise<T> {
+        const entry = await dharma.servicing.getDebtRegistryEntry(id);
+
+        // HACK(kayvon): we don't include the debtor in the debt registry entry so we need to pull
+        // it from the collateralizer.
+        const collateralizer = await dharma.contracts.loadCollateralizerAsync();
+        const debtor = await collateralizer.agreementToCollateralizer.callAsync(id);
+
+        const parameters = dharma.adapters.collateralizedSimpleInterestLoan.unpackParameters(
+            entry.termsContractParameters,
+        );
+
+        const principalSymbol = await dharma.token.getTokenSymbolByIndexAsync(
+            parameters.principalTokenIndex,
+        );
+
+        const collateralSymbol = await dharma.token.getTokenSymbolByIndexAsync(
+            parameters.collateralTokenIndex,
+        );
+
+        const params = {
+            id,
+            principal: TokenAmount.fromRaw(parameters.principalAmount, principalSymbol),
+            collateral: TokenAmount.fromRaw(parameters.collateralAmount, collateralSymbol),
+            interestRate: new InterestRate(parameters.interestRate.toNumber()),
+            termLength: new TimeInterval(
+                parameters.termLength.toNumber(),
+                parameters.amortizationUnit,
+            ),
+            debtor: new EthereumAddress(debtor),
+            creditor: new EthereumAddress(entry.beneficiary),
+        };
+
+        return new this(dharma, params) as T;
+    }
+
+    constructor(protected readonly dharma: Dharma, protected readonly params: LoanParams) {}
+
+    /**
+     *  Returns the loan's data as vanilla JS types.
+     *
+     * @example
+     * const data = loan.getData();
+     *
+     * @returns {LoanData}
+     */
+    public getData(): LoanData {
+        const {
+            id,
+            principal,
+            collateral,
+            interestRate,
+            termLength,
+            debtor,
+            creditor,
+        } = this.params;
+
+        return {
+            id,
+            principalAmount: principal.decimalAmount,
+            principalTokenSymbol: principal.tokenSymbol,
+            collateralAmount: collateral.decimalAmount,
+            collateralTokenSymbol: collateral.tokenSymbol,
+            interestRate: interestRate.percent,
+            termDuration: termLength.amount,
+            termUnit: termLength.getAmortizationUnit(),
+            debtorAddress: debtor.toString(),
+            creditorAddress: creditor.toString(),
+        };
+    }
+
+    /**
+     *  Returns loan data as well as repaid amount and the total expected repayment amount.
+     *
+     * @example
+     * const expandedData = await loan.getExpandedData();
+     * => {
+     *      repaidAmount: 100,
+     *      totalExpectedRepaymentAmount: 250,
+     *      ...
+     *    }
+     *
+     * @returns {Promise<ExpandedLoanData>}
+     */
+    public async getExpandedData(): Promise<ExpandedLoanData> {
+        const data = this.getData();
+
+        const repaidAmount = await this.getRepaidAmount();
+        const totalExpectedRepaymentAmount = await this.getTotalExpectedRepaymentAmount();
+
+        return {
+            ...data,
+            repaidAmount,
+            totalExpectedRepaymentAmount,
+        };
+    }
+
     /**
      * Eventually returns true if the loan's collateral has been either seized
      * by the creditor or returned to the debtor.
@@ -15,7 +144,7 @@ export class Loan {
      */
     public async isCollateralWithdrawn(): Promise<boolean> {
         return this.dharma.adapters.collateralizedSimpleInterestLoan.isCollateralWithdrawn(
-            this.getAgreementId(),
+            this.params.id,
         );
     }
 
@@ -31,7 +160,7 @@ export class Loan {
      */
     public async isCollateralSeizable(): Promise<boolean> {
         return this.dharma.adapters.collateralizedSimpleInterestLoan.canSeizeCollateral(
-            this.getAgreementId(),
+            this.params.id,
         );
     }
 
@@ -61,7 +190,7 @@ export class Loan {
      */
     public async isCollateralReturnable(): Promise<boolean> {
         return this.dharma.adapters.collateralizedSimpleInterestLoan.canReturnCollateral(
-            this.getAgreementId(),
+            this.params.id,
         );
     }
 
@@ -96,10 +225,8 @@ export class Loan {
      * @returns {Promise<number>}
      */
     public async getTotalExpectedRepaymentAmount(): Promise<number> {
-        const agreementId = this.getAgreementId();
-
         const totalExpectedRepaymentAmount = await this.dharma.servicing.getTotalExpectedRepayment(
-            agreementId,
+            this.params.id,
         );
 
         const tokenSymbol = this.params.principal.tokenSymbol;
@@ -158,9 +285,7 @@ export class Loan {
      * @returns {Promise<number>}
      */
     public async getRepaidAmount(): Promise<number> {
-        const agreementId = this.getAgreementId();
-
-        const repaidAmount = await this.dharma.servicing.getValueRepaid(agreementId);
+        const repaidAmount = await this.dharma.servicing.getValueRepaid(this.params.id);
 
         const tokenSymbol = this.params.principal.tokenSymbol;
 
