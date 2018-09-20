7c61a62ec3da5419b3f4f93a14fc0e4cd9d40322
Graeme <graemelearn@protonmail.com>
2018-09-20 16:37:52 -0700

    feat: add DebtOfferData interface

---
+++ src/types/debt_offer_data.ts
@@ -0,0 +1,21 @@
+import { BigNumber } from "../../utils/bignumber";
+
+import { ECDSASignature } from "./ecdsa_signature";
+
+/**
+ * Contains the terms for which creditor would be willing to enter into a debt agreement.
+ */
+export interface DebtOfferData {
+    // The amount that the creditor is willing to lend.
+    principalAmount: BigNumber,
+    // The symbol of acceptance collateral, e.g. "REP".
+    collateralTokenSymbol: string;
+    // The maximum loan-to-value ratio acceptable to this creditor, e.g. 50%.
+    maxLoanToValuePercent: BigNumber;
+    // The maximum time at which the offer will still be valid, as a Unix timestamp.
+    expirationTimestamp: number;
+    // The address of the decision engine that will validate the terms of the offer.
+    decisionEngineAddress: string;
+    // The creditor signs this offer data.
+    creditorSignature: ECDSASignature;
+}
