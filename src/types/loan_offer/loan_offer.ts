6a8065996b09c319f6a92989c11bdfdd4d78d1db
Chris Min <chris@dharma.io>
2018-09-21 14:45:31 -0700

    feat: introduce signAsCreditor to LoanOffer

--- src/types/loan_offer/loan_offer.ts
+++ src/types/loan_offer/loan_offer.ts
@@ -2,6 +2,13 @@ import { Dharma } from "../dharma";
 
 import { DebtOrder, DebtOrderParams } from "../../loan/debt_order";
 
+import { DebtOrderDataWrapper } from "../../wrappers";
+import { ECDSASignature } from "../ecdsa_signature";
+
+import { Web3Utils } from "../../../utils/web3_utils";
+
+const DECISION_ENGINE_ADDRESS = "decisionEngineAddress";
+
 export class LoanOffer extends DebtOrder {
     public static async createAndSignAsCreditor(
         dharma: Dharma,
@@ -13,5 +20,26 @@ export class LoanOffer extends DebtOrder {
         await offer.signAsCreditor(creditor);
 
         return offer;
+    }
+
+    private creditorSignature?: ECDSASignature;
+
+    public async signAsCreditor(creditor?: string): Promise<void> {
+        const loanOfferHash = this.getLoanOfferHash();
+
+        const isMetaMask = !!this.dharma.web3.currentProvider.isMetaMask;
+
+        this.creditorSignature = await this.dharma.sign.signPayloadWithAddress(
+            loanOfferHash,
+            creditor,
+            isMetaMask,
+        );
+    }
+
+    private getLoanOfferHash(): string {
+        return Web3Utils.soliditySHA3(
+            new DebtOrderDataWrapper(this.data).getHash(),
+            DECISION_ENGINE_ADDRESS,
+        );
     }
 }
