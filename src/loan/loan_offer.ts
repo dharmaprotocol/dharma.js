df245f5206f1d3745c622ff7cc64b0dbbfd35d2b
Kayvon Tehranian <ktehranian@gmail.com>
2018-09-20 18:15:54 -0700

    feat: create loan offer class with stubbed out methods

---
+++ src/loan/loan_offer.ts
@@ -0,0 +1,29 @@
+import { Dharma } from "../types/dharma";
+
+export class LoanOffer {
+    public static async create(dharma: Dharma): Promise<LoanOffer> {
+        return new LoanOffer(dharma);
+    }
+
+    public static async load(dharma: Dharma): Promise<LoanOffer> {
+        return new LoanOffer(dharma);
+    }
+
+    private constructor(private readonly dharma: Dharma) {}
+
+    public async accept(): Promise<string> {
+        return "test";
+    }
+
+    public async signAsCreditor(): Promise<void> {
+        return;
+    }
+
+    public async signAsDebtor(): Promise<void> {
+        return;
+    }
+
+    public toJSON(): string {
+        return "json";
+    }
+}
