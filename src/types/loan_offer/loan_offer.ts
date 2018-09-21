3285d14ef43e51d940635eea20d7634d973534d0
Chris Min <chris@dharma.io>
2018-09-21 15:48:47 -0700

    feat: add accept to LoanOffer

--- src/types/loan_offer/loan_offer.ts
+++ src/types/loan_offer/loan_offer.ts
@@ -2,7 +2,7 @@ import { Dharma } from "../dharma";
 
 import { DebtOrder, DebtOrderParams } from "../../loan/debt_order";
 
-import { DebtOrderDataWrapper } from "../../wrappers";
+import { CreditorProxyContract, DebtOrderDataWrapper } from "../../wrappers";
 import { ECDSASignature } from "../ecdsa_signature";
 
 import { Web3Utils } from "../../../utils/web3_utils";
@@ -17,22 +17,38 @@ export class LoanOffer extends DebtOrder {
     ): Promise<LoanOffer> {
         const offer = await LoanOffer.create<LoanOffer>(dharma, params);
 
+        offer.data.creditor = creditor;
+
         await offer.signAsCreditor(creditor);
 
         return offer;
     }
-
-    private creditorSignature?: ECDSASignature;
 
     public async signAsCreditor(creditor?: string): Promise<void> {
         const loanOfferHash = this.getLoanOfferHash();
 
         const isMetaMask = !!this.dharma.web3.currentProvider.isMetaMask;
 
-        this.creditorSignature = await this.dharma.sign.signPayloadWithAddress(
+        this.data.creditorSignature = await this.dharma.sign.signPayloadWithAddress(
             loanOfferHash,
             creditor,
             isMetaMask,
+        );
+    }
+
+    public async accept(): Promise<string> {
+        const creditorProxy = await this.dharma.contracts.loadCreditorProxyContract();
+
+        const debtOrderDataWrapper = new DebtOrderDataWrapper(this.data);
+
+        return creditorProxy.fillDebtOffer.sendTransactionAsync(
+            this.data.creditor,
+            debtOrderDataWrapper.getOrderAddresses(),
+            debtOrderDataWrapper.getOrderValues(),
+            debtOrderDataWrapper.getOrderBytes32(),
+            debtOrderDataWrapper.getSignaturesV(),
+            debtOrderDataWrapper.getSignaturesR(),
+            debtOrderDataWrapper.getSignaturesS(),
         );
     }
 
