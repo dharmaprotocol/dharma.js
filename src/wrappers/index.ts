1a74b27a3f1c75bdc6eba83626d05ebc19d49ec2
Chris Min <chris@dharma.io>
2018-09-26 16:02:56 -0700

    fix: reorder imports alphabetically

--- src/wrappers/index.ts
+++ src/wrappers/index.ts
@@ -1,14 +1,19 @@
 import { BaseContract } from "./contract_wrappers/base_contract_wrapper";
 import { CollateralizedSimpleInterestTermsContractContract } from "./contract_wrappers/collateralized_simple_interest_terms_contract_wrapper";
-import { ERC721CollateralizedSimpleInterestTermsContractContract } from "./contract_wrappers/e_r_c721_collateralized_simple_interest_terms_contract_wrapper";
 import { CollateralizerContract } from "./contract_wrappers/collateralizer_wrapper";
 import { ContractRegistryContract } from "./contract_wrappers/contract_registry_wrapper";
+import { CreditorProxyContract } from "./contract_wrappers/creditor_proxy_wrapper";
 import { DebtKernelContract } from "./contract_wrappers/debt_kernel_wrapper";
 import { DebtRegistryContract } from "./contract_wrappers/debt_registry_wrapper";
 import { DebtTokenContract } from "./contract_wrappers/debt_token_wrapper";
 import { DummyTokenContract } from "./contract_wrappers/dummy_token_wrapper";
+import { ERC721CollateralizedSimpleInterestTermsContractContract } from "./contract_wrappers/e_r_c721_collateralized_simple_interest_terms_contract_wrapper";
+import { ERC721CollateralizerContract } from "./contract_wrappers/e_r_c721_collateralizer_wrapper";
+import { ERC721TokenRegistryContract } from "./contract_wrappers/e_r_c721_token_registry_wrapper";
+import { ERC721TokenContract } from "./contract_wrappers/e_r_c721_token_wrapper";
 import { ERC20Contract } from "./contract_wrappers/erc20_wrapper";
 import { ERC721ReceiverContract } from "./contract_wrappers/erc721_receiver_wrapper";
+import { MintableERC721TokenContract } from "./contract_wrappers/mintable_e_r_c721_token_wrapper";
 import { MockERC721ReceiverContract } from "./contract_wrappers/mock_erc721_receiver_wrapper";
 import { RepaymentRouterContract } from "./contract_wrappers/repayment_router_wrapper";
 import { SimpleInterestTermsContractContract } from "./contract_wrappers/simple_interest_terms_contract_wrapper";
@@ -16,11 +21,6 @@ import { TermsContract } from "./contract_wrappers/terms_contract_wrapper";
 import { TokenRegistryContract } from "./contract_wrappers/token_registry_wrapper";
 import { TokenTransferProxyContract } from "./contract_wrappers/token_transfer_proxy_wrapper";
 import { DebtOrderDataWrapper } from "./debt_order_data_wrapper";
-import { ERC721TokenRegistryContract } from "./contract_wrappers/e_r_c721_token_registry_wrapper";
-import { ERC721TokenContract } from "./contract_wrappers/e_r_c721_token_wrapper";
-import { ERC721CollateralizerContract } from "./contract_wrappers/e_r_c721_collateralizer_wrapper";
-import { MintableERC721TokenContract } from "./contract_wrappers/mintable_e_r_c721_token_wrapper";
-import { CreditorProxyContract } from "./contract_wrappers/creditor_proxy_wrapper";
 
 export type ContractWrapper =
     | DebtKernelContract
