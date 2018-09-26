0b09482e3249c4bbb2d38ef25275b89dfe2de2e2
Chris Min <chris@dharma.io>
2018-09-25 18:29:13 -0700

    feat: introduce signAsCreditor test

--- __test__/integration/loan_offer/loan_offer.spec.ts
+++ __test__/integration/loan_offer/loan_offer.spec.ts
@@ -8,6 +8,7 @@ import { testAcceptAsDebtor } from "./runners/accept_as_debtor";
 import { testAcceptAsProxy } from "./runners/accept_as_proxy";
 import { testCreate } from "./runners/create";
 import { testCreateAndSignAsCreditor } from "./runners/create_and_sign_as_creditor";
+import { testSignAsCreditor } from "./runners/sign_as_creditor";
 
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const dharma = new Dharma(provider);
@@ -15,6 +16,10 @@ const dharma = new Dharma(provider);
 describe("Loan Request (Integration)", () => {
     describe("#create", async () => {
         await testCreate(dharma, VALID_DEBT_ORDER_PARAMS);
+    });
+
+    describe("#signAsCreditor", async () => {
+        await testSignAsCreditor(dharma, VALID_DEBT_ORDER_PARAMS);
     });
 
     describe("#createAndSignAsCreditor", async () => {
