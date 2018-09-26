ce64cfe61a6aeb3aa94a60ae00e1e28459b574de
Chris Min <chris@dharma.io>
2018-09-26 14:43:21 -0700

    refactor: clean up set up of balances and allowances

--- __test__/integration/loan_offer/runners/accept.ts
+++ __test__/integration/loan_offer/runners/accept.ts
@@ -1,14 +1,10 @@
-import * as Web3 from "web3";
-
 import { BigNumber, Dharma } from "../../../../src";
 
 import { LoanOffer } from "../../../../src/types";
 
-import { ContractsAPI } from "../../../../src/apis/";
-
-import { DummyTokenContract } from "../../../../src/wrappers";
-
 import { DebtOrderParams } from "../../../../src/loan/debt_order";
+
+import { setBalancesAndAllowances } from "../utils/set_balances_and_allowances";
 
 // Accounts
 import { ACCOUNTS } from "../../../accounts";
@@ -28,60 +24,10 @@ export async function testAccept(
     describe("passing valid params", () => {
         let loanOffer: LoanOffer;
 
-        let web3: Web3;
-
-        let contractsApi: ContractsAPI;
-
-        let principalToken: DummyTokenContract;
-        let collateralToken: DummyTokenContract;
-
-        beforeAll(async () => {
+        beforeEach(async () => {
             loanOffer = await LoanOffer.createAndSignAsCreditor(dharma, params);
 
-            const provider = new Web3.providers.HttpProvider("http://localhost:8545");
-            web3 = new Web3(provider);
-
-            contractsApi = new ContractsAPI(web3);
-
-            principalToken = await DummyTokenContract.at(
-                (await contractsApi.loadTokenBySymbolAsync(params.principalToken)).address,
-                web3,
-                TX_DEFAULTS,
-            );
-
-            collateralToken = await DummyTokenContract.at(
-                (await contractsApi.loadTokenBySymbolAsync(params.collateralToken)).address,
-                web3,
-                TX_DEFAULTS,
-            );
-        });
-
-        beforeEach(async () => {
-            // set balances
-            await principalToken.setBalance.sendTransactionAsync(
-                CREDITOR.address,
-                new BigNumber(10000000000).times(new BigNumber(10).pow(18)),
-                TX_DEFAULTS,
-            );
-
-            await collateralToken.setBalance.sendTransactionAsync(
-                DEBTOR.address,
-                new BigNumber(10000000000).times(new BigNumber(10).pow(18)),
-                TX_DEFAULTS,
-            );
-
-            // set allowances
-            await Token.setCreditorProxyAllowanceToUnlimited(
-                dharma,
-                params.principalToken,
-                CREDITOR.address,
-            );
-
-            await Token.makeAllowanceUnlimitedIfNecessary(
-                dharma,
-                params.collateralToken,
-                DEBTOR.address,
-            );
+            setBalancesAndAllowances(dharma, params, DEBTOR.address, CREDITOR.address);
         });
 
         test("is accepted by debtor", async () => {
