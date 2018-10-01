121aa044be7e1210e13a2267dc31497c884ce712
Chris Min <chris@dharma.io>
2018-10-01 15:06:06 -0700

    test: improve signAsDebtor tests

--- __test__/integration/max_ltv_loan_offer/runners/sign_as_debtor.ts
+++ __test__/integration/max_ltv_loan_offer/runners/sign_as_debtor.ts
@@ -2,7 +2,7 @@ import { Dharma } from "../../../../src";
 
 import { MaxLTVLoanOffer } from "../../../../src/types";
 
-import { MaxLTVParams } from "../../../../src/types/loan_offer/max_ltv";
+import { MAX_LTV_LOAN_OFFER_ERRORS, MaxLTVParams } from "../../../../src/types/loan_offer/max_ltv";
 
 import { generateSignedPrice } from "../utils/generate_signed_price";
 
@@ -16,16 +16,7 @@ export async function testSignAsDebtor(dharma: Dharma, params: MaxLTVParams) {
 
         let loanOffer: MaxLTVLoanOffer;
 
-        beforeEach(() => {
-            loanOffer = new MaxLTVLoanOffer(dharma, params);
-        });
-
-        test("signs the offer as the debtor if all prerequisites are met", async () => {
-            const isSignedByDebtorBefore = loanOffer.isSignedByDebtor();
-            expect(isSignedByDebtorBefore).toBe(false);
-
-            await loanOffer.signAsCreditor(creditor);
-
+        async function setPrices() {
             const principalTokenAddress = await dharma.contracts.getTokenAddressBySymbolAsync(
                 params.principalToken,
             );
@@ -50,6 +41,19 @@ export async function testSignAsDebtor(dharma: Dharma, params: MaxLTVParams) {
 
             loanOffer.setPrincipalPrice(principalPrice);
             loanOffer.setCollateralPrice(collateralPrice);
+        }
+
+        beforeEach(() => {
+            loanOffer = new MaxLTVLoanOffer(dharma, params);
+        });
+
+        test("signs the offer as the debtor if all prerequisites are met", async () => {
+            const isSignedByDebtorBefore = loanOffer.isSignedByDebtor();
+            expect(isSignedByDebtorBefore).toBe(false);
+
+            await loanOffer.signAsCreditor(creditor);
+
+            await setPrices();
 
             loanOffer.setCollateralAmount(160);
 
@@ -57,6 +61,72 @@ export async function testSignAsDebtor(dharma: Dharma, params: MaxLTVParams) {
 
             const isSignedByDebtorAfter = loanOffer.isSignedByDebtor();
             expect(isSignedByDebtorAfter).toBe(true);
+        });
+
+        describe("should throw", () => {
+            test("when the debtor has already signed", async () => {
+                const isSignedByDebtorBefore = loanOffer.isSignedByDebtor();
+                expect(isSignedByDebtorBefore).toBe(false);
+
+                await loanOffer.signAsCreditor(creditor);
+
+                await setPrices();
+
+                loanOffer.setCollateralAmount(160);
+
+                await loanOffer.signAsDebtor(debtor);
+
+                // second attempt to sign as debtor
+                await expect(loanOffer.signAsDebtor(debtor)).rejects.toThrow(
+                    MAX_LTV_LOAN_OFFER_ERRORS.ALREADY_SIGNED_BY_DEBTOR(),
+                );
+            });
+
+            test("when prices are not set", async () => {
+                const isSignedByDebtorBefore = loanOffer.isSignedByDebtor();
+                expect(isSignedByDebtorBefore).toBe(false);
+
+                await loanOffer.signAsCreditor(creditor);
+
+                loanOffer.setCollateralAmount(160);
+
+                await expect(loanOffer.signAsDebtor(debtor)).rejects.toThrow(
+                    MAX_LTV_LOAN_OFFER_ERRORS.PRICES_NOT_SET(),
+                );
+            });
+
+            test("when the collateral amount is not set", async () => {
+                const isSignedByDebtorBefore = loanOffer.isSignedByDebtor();
+                expect(isSignedByDebtorBefore).toBe(false);
+
+                await loanOffer.signAsCreditor(creditor);
+
+                await setPrices();
+
+                await expect(loanOffer.signAsDebtor(debtor)).rejects.toThrow(
+                    MAX_LTV_LOAN_OFFER_ERRORS.COLLATERAL_AMOUNT_NOT_SET(),
+                );
+            });
+
+            test("when the collateral amount is insufficient", async () => {
+                const isSignedByDebtorBefore = loanOffer.isSignedByDebtor();
+                expect(isSignedByDebtorBefore).toBe(false);
+
+                await loanOffer.signAsCreditor(creditor);
+
+                await setPrices();
+
+                const collateralAmount = 10;
+
+                loanOffer.setCollateralAmount(collateralAmount);
+
+                await expect(loanOffer.signAsDebtor(debtor)).rejects.toThrow(
+                    MAX_LTV_LOAN_OFFER_ERRORS.INSUFFICIENT_COLLATERAL_AMOUNT(
+                        collateralAmount,
+                        params.collateralToken,
+                    ),
+                );
+            });
         });
     });
 }
