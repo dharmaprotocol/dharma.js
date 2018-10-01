12d8b39566c1a682b3300d19a3f9997adbcf987e
Chris Min <chris@dharma.io>
2018-10-01 13:44:12 -0700

    fix: rename file

--- __test__/integration/max_ltv_loan_offer/max_ltv_loan_offer.spec.ts
+++ __test__/integration/max_ltv_loan_offer/max_ltv_loan_offer.spec.ts
@@ -5,7 +5,7 @@ jest.unmock("@dharmaprotocol/contracts");
 import { VALID_MAX_LTV_LOAN_ORDER_PARAMS } from "./scenarios/valid_max_ltv_loan_order_params";
 
 import { testConstructor } from "./runners/constructor";
-import { testCreateAndSignAsCreditor } from "./runners/createAndSignAsCreditor";
+import { testCreateAndSignAsCreditor } from "./runners/create_and_sign_as_creditor";
 
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const dharma = new Dharma(provider);
